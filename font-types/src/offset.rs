9f9bd5f0a54a306b5ae2d5429655c0c749229d63
Colin Rofls <colin@cmyr.net>
2022-01-24 16:03:14 -0500

    Use non-zero types for offset
    This is maybe/probably too fancy by half

--- font-types/src/offset.rs
+++ font-types/src/offset.rs
@@ -1,35 +1,48 @@
-use crate::Uint24;
+//! Offsets to tables
 
-/// Offsets to tables
+use std::num::{NonZeroU16, NonZeroU32};
 
 macro_rules! impl_offset {
-    ($name:ident, $bits:literal, $ty:ty, $null:expr ) => {
+    ($name:ident, $bits:literal, $ty:ty, $rawty:ty) => {
         #[doc = concat!("A", stringify!($bits), "-bit offset to a table.")]
+        ///
+        /// Offsets should generally be represented as `Option<Offset>`, where
+        /// the NULL offset is represented as the `None` case.
         #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
         pub struct $name($ty);
 
         impl $name {
-            /// A NULL offset
-            pub const NULL: Self = Self($null);
-
             /// Create a new offset.
-            pub fn new(raw: $ty) -> Self {
-                Self(raw)
-            }
-
-            /// Returns true if this offset is NULL.
-            pub fn is_null(self) -> bool {
-                self == Self::NULL
+            pub fn new(raw: $rawty) -> Option<Self> {
+                <$ty>::new(raw).map(Self)
             }
 
             /// Return the raw integer value of this offset
-            pub fn to_raw(self) -> $ty {
-                self.0
+            pub fn to_raw(self) -> $rawty {
+                self.0.get()
             }
         }
     };
 }
 
-impl_offset!(Offset16, 16, u16, 0);
-impl_offset!(Offset24, 24, Uint24, Uint24::MIN);
-impl_offset!(Offset32, 32, u32, 0);
+impl_offset!(Offset16, 16, NonZeroU16, u16);
+impl_offset!(Offset32, 32, NonZeroU32, u32);
+
+/// A 24-bit offset to a table.
+///
+/// Offsets should generally be represented as `Option<Offset>`, where
+/// the NULL offset is represented as the `None` case.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Offset24(NonZeroU32);
+
+impl Offset24 {
+    /// Create a new offset.
+    pub fn new(raw: u32) -> Option<Self> {
+        NonZeroU32::new(raw).map(Self)
+    }
+
+    /// Return the raw integer value of this offset
+    pub fn to_raw(self) -> u32 {
+        self.0.get()
+    }
+}
