8c709dfc439a08465e64461fd21f3326943722fa
Colin Rofls <colin@cmyr.net>
2022-02-21 13:29:21 -0500

    Update codegen tool
    - now supports groups
    - uses new types (BigEndian everything)
    - items are indicated with '@' (useless now but maybe we add more type
      info later)

--- font-types-macro/src/bin/codegen.rs
+++ font-types-macro/src/bin/codegen.rs
@@ -7,20 +7,22 @@
 //!
 //!
 //! ```
-//! Gpos1_0
+//! { // this is a 'group'
+//! @Gpos1_0
 //! uint16      majorVersion       Major version of the GPOS table, = 1
 //! uint16      minorVersion       Minor version of the GPOS table, = 0
 //! Offset16    scriptListOffset   Offset to ScriptList table, from beginning of GPOS table
 //! Offset16    featureListOffset  Offset to FeatureList table, from beginning of GPOS table
 //! Offset16    lookupListOffset   Offset to LookupList table, from beginning of GPOS table
 //!
-//! Gpos1_1
+//! @Gpos1_1
 //! uint16      majorVersion            Major version of the GPOS table, = 1
 //! uint16      minorVersion            Minor version of the GPOS table, = 1
 //! Offset16    scriptListOffset        Offset to ScriptList table, from beginning of GPOS table
 //! Offset16    featureListOffset       Offset to FeatureList table, from beginning of GPOS table
 //! Offset16    lookupListOffset        Offset to LookupList table, from beginning of GPOS table
 //! Offset32    featureVariationsOffset Offset to FeatureVariations table, from beginning of GPOS table (may be NULL)
+//! }
 //! ```
 //!
 //! - different records/tables are separated by newlines.
@@ -30,41 +32,87 @@
 //! *limitations:* this doesn't handle lifetimes, and doesn't generate annotations.
 //! You will need to clean up the output.
 
-use std::fmt::Write;
+use std::{fmt::Write, ops::Deref};
 
 macro_rules! exit_with_msg {
-    ($disp:expr) => {{
+    ($disp:expr, $line:expr) => {{
         eprintln!("ERROR: {}", $disp);
+        eprintln!("Line {}: '{}'", 1 + $line.number, $line.text);
         std::process::exit(1);
     }};
 }
 
-static MACRO_CALL: &str = "toy_table_macro::tables!";
+static MACRO_CALL: &str = "font_types::tables!";
+
+/// a wrapper around a line, so we can report errors with line numbers
+struct Line<'a> {
+    text: &'a str,
+    number: usize,
+}
+
+impl Deref for Line<'_> {
+    type Target = str;
+    fn deref(&self) -> &<Self as Deref>::Target {
+        self.text
+    }
+}
 
 fn main() {
     let in_path = std::env::args().nth(1).expect("expected path argument");
     let input = std::fs::read_to_string(in_path).expect("failed to read path");
-    let mut lines = input.lines().map(str::trim).filter(|l| !l.starts_with('#'));
-    println!("{} {{", MACRO_CALL);
-    while let Some(item) = generate_one_item(&mut lines) {
-        println!("{}", item);
+    let mut lines = input
+        .lines()
+        .enumerate()
+        .map(|(number, text)| Line {
+            text: text.trim(),
+            number,
+        })
+        .filter(|l| !l.starts_with('#'));
+
+    while let Some(group) = generate_group(&mut lines) {
+        println!("{} {{", MACRO_CALL);
+        println!("{}", group);
+        println!("}}");
     }
-    println!("}}");
+}
+
+/// Generate a group of items. This is multiple items within a pair of brackets,
+/// which will share a single macro invocation.
+fn generate_group<'a>(lines: impl Iterator<Item = Line<'a>>) -> Option<String> {
+    let mut lines = lines.skip_while(|s| s.is_empty()).peekable();
+    let brace = lines.next()?;
+
+    let mut result = String::new();
+
+    if !brace.starts_with('{') {
+        exit_with_msg!("expected opening brace", brace);
+    }
+
+    let mut lines = lines.take_while(|line| !line.starts_with('}'));
+
+    while let Some(item) = generate_one_item(&mut lines) {
+        if !result.is_empty() {
+            result.push('\n');
+        }
+        result.push_str(&item);
+    }
+    Some(result)
 }
 
 /// parse a single table or record.
 ///
 /// Returns `Some` on success, `None` if there are no more items, and terminates
 /// if something goes wrong.
-fn generate_one_item<'a>(lines: impl Iterator<Item = &'a str>) -> Option<String> {
+fn generate_one_item<'a>(lines: impl Iterator<Item = Line<'a>>) -> Option<String> {
     let mut lines = lines.skip_while(|line| line.is_empty());
 
-    let name = match lines.next() {
-        Some(line) if line.split_whitespace().nth(1).is_none() => line,
-        Some(line) => exit_with_msg!(format!("expected table or record name, found '{}'", line)),
-        _ => return None,
+    let decl_line = match lines.next() {
+        Some(line) if line.starts_with('@') => line,
+        Some(line) => exit_with_msg!("expected table or record name", line),
+        None => return None,
     };
 
+    let name = decl_line.trim_matches('@');
     let fields = lines.map_while(parse_field).collect::<Vec<_>>();
     let lifetime_str = if fields.iter().any(|x| x.maybe_count.is_some()) {
         "<'a>"
@@ -75,7 +123,7 @@ fn generate_one_item<'a>(lines: impl Iterator<Item = &'a str>) -> Option<String>
     for line in &fields {
         writeln!(&mut result, "{}", line).unwrap();
     }
-    result.push_str("}\n");
+    result.push('}');
     Some(result)
 }
 
@@ -102,17 +150,14 @@ impl<'a> std::fmt::Display for Field<'a> {
     }
 }
 
-fn parse_field(line: &str) -> Option<Field> {
+fn parse_field(line: Line) -> Option<Field> {
     if line.is_empty() {
         return None;
     }
-    let mut iter = line.splitn(3, |c: char| c.is_ascii_whitespace());
+    let mut iter = line.text.splitn(3, |c: char| c.is_ascii_whitespace());
     let (typ, ident, comment) = match (iter.next(), iter.next(), iter.next()) {
         (Some(a), Some(b), Some(c)) => (a, b, c),
-        _ => exit_with_msg!(format!(
-            "line could not be parsed as type/name/comment: '{}'",
-            line
-        )),
+        _ => exit_with_msg!("line could not be parsed as type/name/comment", line),
     };
     let typ = normalize_type(typ);
     let (name, maybe_count) = split_ident(ident);
@@ -137,17 +182,23 @@ fn split_ident(input: &str) -> (&str, Option<&str>) {
 
 fn normalize_type(input: &str) -> &str {
     match input {
-        "uint8" => "Uint8",
-        "uint16" => "Uint16",
-        "uint24" => "Uint24",
-        "uint32" => "Uint32",
-        "int8" => "Int8",
-        "int16" => "Int16",
-        "int32" => "Int32",
-        "FWORD" => "FWord",
-        "UFWORD" => "UfWord",
-        "F2DOT14" => "F2Dot14",
-        "LONGDATETIME" => "LongDateTime",
+        "uint8" => "BigEndian<u8>",
+        "uint16" => "BigEndian<u16>",
+        "uint24" => "BigEndian<Uint24>",
+        "uint32" => "BigEndian<u32>",
+        "int8" => "BigEndian<i8>",
+        "int16" => "BigEndian<i16>",
+        "int32" => "BigEndian<i32>",
+        "FWORD" => "BigEndian<FWord>",
+        "UFWORD" => "BigEndian<UfWord>",
+        "F2DOT14" => "BigEndian<F2Dot14>",
+        "LONGDATETIME" => "BigEndian<LongDateTime>",
+        "Version16Dot16" => "BigEndian<Version16Dot16>",
+        "Fixed" => "BigEndian<Fixed>",
+        "Tag" => "BigEndian<Tag>",
+        "Offset16" => "BigEndian<Offset16>",
+        "Offset24" => "BigEndian<Offset24>",
+        "Offset32" => "BigEndian<Offset32>",
         other => other,
     }
 }
