9cbb0e30b6a148e8d310d7e32faa8bbdd93bf7b1
Colin Rofls <colin@cmyr.net>
2022-09-08 12:58:51 -0400

    [inspect] Add query support to the inspect tool
    This lets the user provide a path through the object graph, which we
    will traverse and print.

--- read-fonts/examples/inspect.rs
+++ read-fonts/examples/inspect.rs
@@ -3,29 +3,26 @@
 //! This accepts command line arguments similar to what is present in ttx,
 //! although it does not produce xml output.
 
-use std::{collections::HashSet, str::FromStr};
+use std::{borrow::Cow, collections::HashSet, fmt::Write, str::FromStr};
 
 use font_types::Tag;
 use read_fonts::{
-    traversal::{FieldType, OffsetType, ResolvedOffset, SomeArray, SomeTable},
-    FontData, FontRef, TableProvider,
+    traversal::{Field, FieldType, OffsetType, ResolvedOffset, SomeArray, SomeTable},
+    FontData, FontRef, ReadError, TableProvider,
 };
 
 fn main() -> Result<(), Error> {
-    let args = match flags::Args::from_env() {
-        Ok(args) => args,
-        Err(e) => {
-            eprintln!("{e}");
-            std::process::exit(1);
-        }
-    };
-
+    let args = flags::Args::from_env().map_err(|e| Error(e.to_string()))?;
     let bytes = std::fs::read(&args.input).unwrap();
     let data = FontData::new(&bytes);
     let font = FontRef::new(data).unwrap();
     if args.list {
         list_tables(&font);
         return Ok(());
+    }
+
+    if let Some(query) = &args.query {
+        return print_query(&font, query).map_err(Error);
     }
 
     let filter = TableFilter::from_args(&args)?;
@@ -83,21 +80,144 @@ fn print_tables(font: &FontRef, filter: &TableFilter) {
     }
 }
 
-fn print_table(font: &FontRef, tag: Tag) {
+fn get_some_table<'a>(
+    font: &FontRef<'a>,
+    tag: Tag,
+) -> Result<Box<dyn SomeTable<'a> + 'a>, ReadError> {
     match tag {
-        read_fonts::tables::gpos::TAG => fancy_print_table(&font.gpos().unwrap()).unwrap(),
-        read_fonts::tables::cmap::TAG => fancy_print_table(&font.cmap().unwrap()).unwrap(),
-        read_fonts::tables::gdef::TAG => fancy_print_table(&font.gdef().unwrap()).unwrap(),
-        read_fonts::tables::glyf::TAG => fancy_print_table(&font.glyf().unwrap()).unwrap(),
-        read_fonts::tables::head::TAG => fancy_print_table(&font.head().unwrap()).unwrap(),
-        read_fonts::tables::hhea::TAG => fancy_print_table(&font.hhea().unwrap()).unwrap(),
-        read_fonts::tables::hmtx::TAG => fancy_print_table(&font.hmtx().unwrap()).unwrap(),
-        read_fonts::tables::loca::TAG => fancy_print_table(&font.loca(None).unwrap()).unwrap(),
-        read_fonts::tables::maxp::TAG => fancy_print_table(&font.maxp().unwrap()).unwrap(),
-        read_fonts::tables::name::TAG => fancy_print_table(&font.name().unwrap()).unwrap(),
-        read_fonts::tables::post::TAG => fancy_print_table(&font.post().unwrap()).unwrap(),
-        _ => println!("unknown tag {tag}"),
+        read_fonts::tables::gpos::TAG => font.gpos().map(|x| Box::new(x) as _),
+        read_fonts::tables::cmap::TAG => font.cmap().map(|x| Box::new(x) as _),
+        read_fonts::tables::gdef::TAG => font.gdef().map(|x| Box::new(x) as _),
+        read_fonts::tables::glyf::TAG => font.glyf().map(|x| Box::new(x) as _),
+        read_fonts::tables::head::TAG => font.head().map(|x| Box::new(x) as _),
+        read_fonts::tables::hhea::TAG => font.hhea().map(|x| Box::new(x) as _),
+        read_fonts::tables::hmtx::TAG => font.hmtx().map(|x| Box::new(x) as _),
+        read_fonts::tables::loca::TAG => font.loca(None).map(|x| Box::new(x) as _),
+        read_fonts::tables::maxp::TAG => font.maxp().map(|x| Box::new(x) as _),
+        read_fonts::tables::name::TAG => font.name().map(|x| Box::new(x) as _),
+        read_fonts::tables::post::TAG => font.post().map(|x| Box::new(x) as _),
+        _ => Err(ReadError::TableIsMissing(tag)),
     }
+}
+
+fn print_table(font: &FontRef, tag: Tag) {
+    match get_some_table(font, tag) {
+        Ok(table) => fancy_print_table(&table).unwrap(),
+        Err(err) => println!("{tag}: Error '{err}'"),
+    }
+}
+
+fn print_query(font: &FontRef, query: &Query) -> Result<(), String> {
+    let table = match get_some_table(font, query.tag) {
+        Ok(table) => table,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    match query.elements.split_first() {
+        Some((QueryElement::Field(name), rest)) => {
+            let field = get_field(&table, name)?;
+            let mut used_path = vec![QueryElement::Field(field.name.to_string())];
+            let target = find_query_recursive(field.typ, rest, &mut used_path)?;
+            print_used_query(query, &used_path);
+            println!("found {}", field_type_name(&target));
+            println!();
+            print_field(target).map_err(|e| format!("print failed: '{e}'"))
+        }
+        Some((QueryElement::Index(_), _)) => Err("tables cannot be indexed".into()),
+        None => {
+            fancy_print_table(&table).unwrap();
+            Ok(())
+        }
+    }
+}
+
+fn print_used_query(query: &Query, used: &[QueryElement]) {
+    let tag = query.tag;
+    let mut q_string = format!("query {tag}");
+    let mut m_string = format!("match {tag}");
+
+    for (q_elem, m_elem) in query.elements.iter().zip(used) {
+        match (q_elem, m_elem) {
+            (QueryElement::Field(name1), QueryElement::Field(name2)) => {
+                let padding = name1.len().max(name2.len());
+                write!(&mut q_string, ".{name1:padding$}").unwrap();
+                write!(&mut m_string, ".{name2:padding$}").unwrap();
+            }
+            (QueryElement::Index(idx), QueryElement::Index(_)) => {
+                write!(&mut q_string, "[{idx}]").unwrap();
+                write!(&mut m_string, "[{idx}]").unwrap();
+            }
+            _ => panic!("this should not happen"),
+        }
+    }
+
+    println!("{q_string}");
+    println!("{m_string}");
+}
+
+fn find_query_recursive<'a>(
+    current: FieldType<'a>,
+    query_path: &[QueryElement],
+    used_path: &mut Vec<QueryElement>,
+) -> Result<FieldType<'a>, String> {
+    let (next, rest) = match query_path.split_first() {
+        Some(thing) => thing,
+        None => return Ok(current),
+    };
+
+    match next {
+        QueryElement::Field(name) => {
+            let field = match current {
+                FieldType::ResolvedOffset(ResolvedOffset { target, .. }) => match target {
+                    Ok(table) => get_field(&table, name),
+                    Err(err) => Err(format!("Error reading offset for field '{name}': '{err}'")),
+                },
+                FieldType::Record(record) => get_field(&record, name),
+                _ => Err(format!(
+                    "No field '{name}' on type '{}'",
+                    field_type_name(&current)
+                )),
+            }?;
+            used_path.push(QueryElement::Field(field.name.to_string()));
+            find_query_recursive(field.typ, rest, used_path)
+        }
+        QueryElement::Index(idx) => {
+            let field = match current {
+                FieldType::Array(arr) => arr
+                    .get(*idx as usize)
+                    .ok_or_else(|| format!("index {idx} out of bounds for array")),
+                FieldType::OffsetArray(arr) => arr
+                    .iter()
+                    .nth(*idx as usize)
+                    .ok_or_else(|| format!("index {idx} out of bounds for array")),
+                _ => Err(format!(
+                    "Index provided but field type '{}' is not indexable",
+                    field_type_name(&current)
+                )),
+            }?;
+            used_path.push(next.clone());
+            find_query_recursive(field, rest, used_path)
+        }
+    }
+}
+
+fn get_field<'a>(table: &(dyn SomeTable<'a> + 'a), name: &str) -> Result<Field<'a>, String> {
+    let mut result = None;
+    for field in table.iter() {
+        if ascii_fuzzy_match(name, field.name) {
+            match result.take() {
+                None => result = Some(field),
+                Some(prev) => {
+                    return Err(format!(
+                        "Error: ambiguous query path '{name}' (matches '{}' and '{}')",
+                        prev.name, field.name
+                    ))
+                }
+            }
+        }
+    }
+
+    result.ok_or_else(|| format!("{} contains no field '{name}'", table.type_name()))
 }
 
 enum TableFilter {
@@ -324,6 +444,19 @@ fn fancy_print_table<'a>(table: &(dyn SomeTable<'a> + 'a)) -> std::io::Result<()
     PrintTable(table).print(&mut formatter)
 }
 
+fn print_field(field: FieldType) -> std::io::Result<()> {
+    let stdout = std::io::stdout();
+    let mut locked = stdout.lock();
+    let mut formatter = PrettyPrinter {
+        depth: 0,
+        writer: &mut locked,
+        indent_size: 2,
+    };
+
+    formatter.print_field(&field)?;
+    formatter.print_newline()
+}
+
 pub trait Print {
     fn print(&self, printer: &mut PrettyPrinter) -> std::io::Result<()>;
 }
@@ -375,7 +508,87 @@ GDEF: Gdef
   item_var_store_offset: None,
 */
 
+#[derive(Clone, Debug)]
+pub struct Query {
+    tag: Tag,
+    elements: Vec<QueryElement>,
+}
+
+#[derive(Debug, Clone)]
+enum QueryElement {
+    Field(String),
+    Index(u32),
+}
+
+impl FromStr for Query {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut components = s.split('.');
+        let tag = match components.next() {
+            None => Err("Query string should be non-empty".into()),
+            Some(s) => s.parse::<Tag>().map_err(|s| s.to_string()),
+        }?;
+
+        let elements = components
+            .map(|comp| match comp.chars().next() {
+                Some('0'..='9') => comp
+                    .parse::<u32>()
+                    .map_err(|_| format!("invalid index '{comp}'"))
+                    .map(QueryElement::Index),
+                Some(_) => Ok(QueryElement::Field(comp.into())),
+                None => Err("Empty query elements are not allowed".into()),
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Query { tag, elements })
+    }
+}
+
+fn field_type_name(field_type: &FieldType) -> Cow<'static, str> {
+    match field_type {
+        FieldType::I8(_) => "i8".into(),
+        FieldType::U8(_) => "u8".into(),
+        FieldType::I16(_) => "i16".into(),
+        FieldType::U16(_) => "u16".into(),
+        FieldType::I32(_) => "i32".into(),
+        FieldType::U32(_) => "u32".into(),
+        FieldType::U24(_) => "u24".into(),
+        FieldType::Tag(_) => "Tag".into(),
+        FieldType::FWord(_) => "FWord".into(),
+        FieldType::UfWord(_) => "UfWord".into(),
+        FieldType::MajorMinor(_) => "MajorMinor".into(),
+        FieldType::Version16Dot16(_) => "Version16Dot16".into(),
+        FieldType::F2Dot14(_) => "F2Dot14".into(),
+        FieldType::Fixed(_) => "Fixed".into(),
+        FieldType::LongDateTime(_) => "LongDateTime".into(),
+        FieldType::GlyphId(_) => "GlyphId".into(),
+        FieldType::Array(_) | FieldType::OffsetArray(_) => "Array".into(),
+        FieldType::Record(record) => record.type_name().to_string().into(),
+        FieldType::ValueRecord(_) => "ValueRecord".into(),
+        FieldType::ResolvedOffset(ResolvedOffset {
+            target: Ok(table), ..
+        }) => table.type_name().to_string().into(),
+        FieldType::ResolvedOffset(_) | FieldType::BareOffset(_) => "Offset".into(),
+        FieldType::None => "None".into(),
+    }
+}
+
+/// returns `true` if every byte in the query is present in the field, in the same order.
+fn ascii_fuzzy_match(query: &str, field: &str) -> bool {
+    let mut fld_pos = 0;
+    //let mut hits = 0;
+    for query_byte in query.bytes().map(|b| b.to_ascii_lowercase()) {
+        match field.bytes().skip(fld_pos).position(|b| b == query_byte) {
+            Some(pos) => fld_pos += pos,
+            None => return false,
+        }
+    }
+    true
+}
+
 mod flags {
+    use super::Query;
     use std::path::PathBuf;
 
     xflags::xflags! {
@@ -384,6 +597,7 @@ mod flags {
             required input: PathBuf
             {
                 optional -l, --list
+                optional -q, --query query: Query
                 optional -t, --tables include: String
                 optional -x, --exclude exclude: String
             }
