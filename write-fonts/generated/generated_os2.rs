2100164d229824b90b01a230298851ed998ef2a3
Andy John <andyj@google.com>
2022-10-19 11:20:43 -0700

    Gen files.

--- write-fonts/generated/generated_os2.rs
+++ write-fonts/generated/generated_os2.rs
@@ -8,43 +8,101 @@ use crate::codegen_prelude::*;
 /// [`os2`](https://docs.microsoft.com/en-us/typography/opentype/spec/os2)
 #[derive(Clone, Debug)]
 pub struct Os2 {
+    /// The Average Character Width parameter specifies the arithmetic average
+    /// of the escapement (width) of all non-zero width glyphs in the font.
     pub x_avg_char_width: i16,
+    /// Indicates the visual weight (degree of blackness or thickness of
+    /// strokes) of the characters in the font. Values from 1 to 1000 are valid.
     pub us_weight_class: u16,
+    /// Indicates a relative change from the normal aspect ratio (width to
+    /// height ratio) as specified by a font designer for the glyphs in a font.
     pub us_width_class: u16,
+    /// Indicates font embedding licensing rights for the font.
     pub fs_type: u16,
+    /// The recommended horizontal size in font design units for subscripts for
+    /// this font.
     pub y_subscript_x_size: i16,
+    /// The recommended vertical size in font design units for subscripts for
+    /// this font.
     pub y_subscript_y_size: i16,
+    /// The recommended horizontal offset in font design units for subscripts
+    /// for this font.
     pub y_subscript_x_offset: i16,
+    /// The recommended vertical offset in font design units for subscripts
+    /// for this font.
     pub y_subscript_y_offset: i16,
+    /// The recommended horizontal size in font design units for superscripts
+    /// for this font.
     pub y_superscript_x_size: i16,
+    /// The recommended vertical size in font design units for superscripts
+    /// for this font.
     pub y_superscript_y_size: i16,
+    /// The recommended horizontal offset in font design units for superscripts
+    /// for this font.
     pub y_superscript_x_offset: i16,
+    /// The recommended vertical offset in font design units for superscripts
+    /// for this font.
     pub y_superscript_y_offset: i16,
+    /// Thickness of the strikeout stroke in font design units.
     pub y_strikeout_size: i16,
+    /// The position of the top of the strikeout stroke relative to the
+    /// baseline in font design units.
     pub y_strikeout_position: i16,
+    /// This parameter is a classification of font-family design.
     pub s_family_class: i16,
+    /// Additional specifications are required for PANOSE to classify non-Latin
+    /// character sets.
     pub panose_10: u8,
+    /// Unicode Character Range (bits 0-31).
     pub ul_unicode_range_1: u32,
+    /// Unicode Character Range (bits 32-63).
     pub ul_unicode_range_2: u32,
+    /// Unicode Character Range (bits 64-95).
     pub ul_unicode_range_3: u32,
+    /// Unicode Character Range (bits 96-127).
     pub ul_unicode_range_4: u32,
+    /// The four-character identifier for the vendor of the given type face.
     pub ach_vend_id: Tag,
+    /// Contains information concerning the nature of the font patterns.
     pub fs_selection: u16,
+    /// The minimum Unicode index (character code) in this font.
     pub us_first_char_index: u16,
+    /// The maximum Unicode index (character code) in this font.
     pub us_last_char_index: u16,
+    /// The typographic ascender for this font.
     pub s_typo_ascender: i16,
+    /// The typographic descender for this font.
     pub s_typo_decender: i16,
+    /// The typographic line gap for this font.
     pub s_typo_line_gap: i16,
+    /// The “Windows ascender” metric. This should be used to specify the
+    /// height above the baseline for a clipping region.
     pub us_win_ascent: u16,
+    /// The “Windows descender” metric. This should be used to specify the
+    /// vertical extend below the baseline for a clipping region.
     pub us_win_descent: u16,
+    /// Code page character range bits 0-31.
     pub ul_code_page_range_1: Option<u32>,
+    /// Code page character range bits 32-63.
     pub ul_code_page_range_2: Option<u32>,
+    /// This metric specifies the distance between the baseline and the
+    /// approximate height of non-ascending lowercase letters measured in
+    /// FUnits.
     pub sx_height: Option<i16>,
+    /// This metric specifies the distance between the baseline and the
+    /// approximate height of uppercase letters measured in FUnits.
     pub s_cap_height: Option<i16>,
+    /// This is the Unicode code point, in UTF-16 encoding, of a character that
+    /// can be used for a default glyph.
     pub us_default_char: Option<u16>,
+    /// his is the Unicode code point, in UTF-16 encoding, of a character that
+    /// can be used as a default break character.
     pub us_break_char: Option<u16>,
+    /// This field is used for fonts with multiple optical styles.
     pub us_max_context: Option<u16>,
+    /// This field is used for fonts with multiple optical styles.
     pub us_lower_optical_point_size: Option<u16>,
+    /// This field is used for fonts with multiple optical styles.
     pub us_upper_optical_point_size: Option<u16>,
 }
 
