f0619074c05c8298fb4f35df2fe6ea7c6f7688db
Colin Rofls <colin@cmyr.net>
2022-10-27 10:38:26 -0400

    Add STAT tests and vazirmatn test input
    This test input is intended to include examples of a variety of
    variable font tables, and to be reused in future tests for avar/gvar
    &co.

--- read-fonts/src/tables/stat.rs
+++ read-fonts/src/tables/stat.rs
@@ -6,3 +6,34 @@ use font_types::Tag;
 pub const TAG: Tag = Tag::new(b"STAT");
 
 include!("../../generated/generated_stat.rs");
+
+#[cfg(test)]
+mod tests {
+    use font_types::Fixed;
+
+    use super::*;
+    use crate::test_data::stat as test_data;
+
+    #[test]
+    fn smoke_test() {
+        let table = test_data::vazirmatn();
+        assert_eq!(table.design_axis_count(), 1);
+        let axis_record = &table.design_axes().unwrap()[0];
+        assert_eq!(axis_record.axis_tag(), Tag::new(b"wght"));
+        assert_eq!(axis_record.axis_name_id(), 257);
+        assert_eq!(axis_record.axis_ordering(), 0);
+        let axis_values = table.offset_to_axis_values().unwrap();
+        let axis_values = axis_values
+            .axis_values()
+            .map(|x| x.unwrap())
+            .collect::<Vec<_>>();
+
+        assert_eq!(axis_values.len(), 3);
+        let last = &axis_values[2];
+        if let AxisValue::Format1(table) = last {
+            assert_eq!(table.axis_index(), 0);
+            assert_eq!(table.value_name_id(), 264);
+            assert_eq!(table.value(), Fixed::from_f64(700.0));
+        }
+    }
+}
