38a345784d569e4fe7ec9c1ee7d7b0682413f33e
Chad Brokaw <cbrokaw@gmail.com>
2022-11-07 18:25:04 -0500

    add methods to map codepoints for cmap 4/12

--- read-fonts/src/tables/cmap.rs
+++ read-fonts/src/tables/cmap.rs
@@ -6,3 +6,89 @@ use font_types::Tag;
 pub const TAG: Tag = Tag::new(b"cmap");
 
 include!("../../generated/generated_cmap.rs");
+
+impl<'a> Cmap<'a> {
+    /// Maps a codepoint to a nominal glyph identifier using the first
+    /// available subtable that provides a valid mapping.
+    pub fn map_codepoint(&self, codepoint: u32) -> Option<GlyphId> {
+        for record in self.encoding_records() {
+            if let Ok(subtable) = record.subtable(self.offset_data()) {
+                if let Some(gid) = match subtable {
+                    CmapSubtable::Format4(format4) => format4.map_codepoint(codepoint),
+                    CmapSubtable::Format12(format12) => format12.map_codepoint(codepoint),
+                    _ => None,
+                } {
+                    return Some(gid);
+                }
+            }
+        }
+        None
+    }
+}
+
+impl<'a> Cmap4<'a> {
+    /// Maps a codepoint to a nominal glyph identifier.
+    pub fn map_codepoint(&self, codepoint: u32) -> Option<GlyphId> {
+        if codepoint >= 65535 {
+            return None;
+        }
+        let codepoint = codepoint as u16;
+        let mut lo = 0;
+        let mut hi = self.seg_count_x2() as usize / 2;
+        let start_codes = self.start_code();
+        let end_codes = self.end_code();
+        while lo < hi {
+            let i = (lo + hi) / 2;
+            let start_code = start_codes.get(i)?.get();
+            if codepoint < start_code {
+                hi = i;
+            } else if codepoint > end_codes.get(i)?.get() {
+                lo = i + 1;
+            } else {
+                let deltas = self.id_delta();
+                let range_offsets = self.id_range_offsets();
+                let delta = deltas.get(i)?.get() as i32;
+                let range_offset = range_offsets.get(i)?.get() as usize;
+                if range_offset == 0 {
+                    return Some(GlyphId::new((codepoint as i32 + delta) as u16));
+                }
+                // sigh
+                let mut offset = range_offset / 2 + (codepoint - start_code) as usize;
+                offset = offset.saturating_sub(range_offsets.len() - i);
+                let gid = self.glyph_id_array().get(offset)?.get();
+                if gid != 0 {
+                    return Some(GlyphId::new((gid as i32 + delta) as u16));
+                } else {
+                    return None;
+                }
+            }
+        }
+        None
+    }
+}
+
+impl<'a> Cmap12<'a> {
+    /// Maps a codepoint to a nominal glyph identifier.
+    pub fn map_codepoint(&self, codepoint: u32) -> Option<GlyphId> {
+        let groups = self.groups();
+        let mut lo = 0;
+        let mut hi = groups.len();
+        while lo < hi {
+            let i = (lo + hi) / 2;
+            let group = groups.get(i)?;
+            if codepoint < group.start_char_code() {
+                hi = i;
+            } else if codepoint > group.end_char_code() {
+                lo = i + 1;
+            } else {
+                return Some(GlyphId::new(
+                    (group
+                        .start_glyph_id()
+                        .wrapping_add(codepoint.wrapping_sub(group.start_char_code())))
+                        as u16,
+                ));
+            }
+        }
+        None
+    }
+}
