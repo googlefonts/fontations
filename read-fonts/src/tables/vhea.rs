60f238ca500841018696ba9d3ae5e06ee733f6da
Cosimo Lupo <clupo@google.com>
2022-11-07 16:03:42 +0000

    Add vhea, use the same table struct for both horizontal/vertical header tables
    Part of https://github.com/googlefonts/fontations/issues/103

---
+++ read-fonts/src/tables/vhea.rs
@@ -0,0 +1,6 @@
+//! the [vhea (Horizontal Header)](https://docs.microsoft.com/en-us/typography/opentype/spec/hhea) table
+
+use font_types::Tag;
+
+/// 'vhea'
+pub const TAG: Tag = Tag::new(b"vhea");
