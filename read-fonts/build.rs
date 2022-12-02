e7210e16517c75888decd4a2875fad6518babf5d
Colin Rofls <colin@cmyr.net>
2022-12-02 15:49:54 -0500

    Ensure include_bytes! works when packaged
    Instead of directly including these files, we now have a build sript
    that will copy the files into a build directory *if they exist*, and
    will write an empty file in that location if they do not.
    
    This ensures that include_bytes! always points to a file that exists,
    which means that we will compile as a package.

---
+++ read-fonts/build.rs
@@ -0,0 +1,28 @@
+//! build script for read-fonts.
+//!
+//! This copies certain test files into OUT_DIR if they exist, or writes
+//! empty files if they do not exist.
+//!
+//! This means that the package can still compile even if these files are
+//! missing, such as when packaged for crates.io
+
+use std::path::Path;
+
+static FILES_TO_MOVE: &[&str] = &[
+    "../resources/test_fonts/ttf/linear_gradient_rect_colr_1.ttf",
+    "../resources/test_fonts/ttf/simple_glyf.ttf",
+    "../resources/test_fonts/ttf/vazirmatn_var_trimmed.ttf",
+];
+
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir);
+    for path in FILES_TO_MOVE.iter().map(Path::new) {
+        let out_path = out_dir.join(path.file_name().unwrap());
+        if path.exists() {
+            std::fs::copy(path, out_path).unwrap();
+        } else {
+            std::fs::write(out_path, []).unwrap();
+        }
+    }
+}
