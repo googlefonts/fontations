8554c897048905e3c6772e1a3e7d4a128d7ea8d9
Colin Rofls <colin@cmyr.net>
2022-12-07 14:45:27 -0500

    Make test_helpers::BeBuffer work like a builder
    that is, it returns `Self` from the various mutating methods.

--- read-fonts/src/tables/head.rs
+++ read-fonts/src/tables/head.rs
@@ -9,15 +9,15 @@ mod tests {
 
     #[test]
     fn smoke_text() {
-        let mut buf = BeBuffer::new();
-        buf.extend([1u16, 0u16]);
-        buf.push(Fixed::from_f64(2.8));
-        buf.extend([42u32, 0x5f0f3cf5]);
-        buf.extend([16u16, 4096]); // flags, upm
-        buf.extend([LongDateTime::new(-500), LongDateTime::new(101)]);
-        buf.extend([-100i16, -50, 400, 711]);
-        buf.extend([0u16, 12]); // mac_style / ppem
-        buf.extend([2i16, 1, 0]);
+        let buf = BeBuffer::new()
+            .extend([1u16, 0u16])
+            .push(Fixed::from_f64(2.8))
+            .extend([42u32, 0x5f0f3cf5])
+            .extend([16u16, 4096]) // flags, upm
+            .extend([LongDateTime::new(-500), LongDateTime::new(101)])
+            .extend([-100i16, -50, 400, 711])
+            .extend([0u16, 12]) // mac_style / ppem
+            .extend([2i16, 1, 0]);
 
         let head = super::Head::read(buf.font_data()).unwrap();
         assert_eq!(head.version(), MajorMinor::VERSION_1_0);
