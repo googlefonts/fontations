3fefff8220d556e03a2a144ba74824af58e2a0a8
Rod S <rsheeter@google.com>
2023-03-10 21:48:56 -0800

    Enable writing cmap, fully default for now.

---
+++ write-fonts/src/tables/cmap.rs
@@ -0,0 +1,10 @@
+//! the [cmap] table
+//!
+//! [cmap]: https://docs.microsoft.com/en-us/typography/opentype/spec/cmap
+
+include!("../../generated/generated_cmap.rs");
+
+
+#[cfg(test)]
+mod tests {
+}
