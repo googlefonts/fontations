1adea907d619ae77541ee86a94018a689ab11935
Rod S <rsheeter@google.com>
2023-03-13 15:52:10 -0700

    Add a high level API for generation of cmap

--- write-fonts/src/tables/cmap.rs
+++ write-fonts/src/tables/cmap.rs
@@ -2,62 +2,142 @@
 //!
 //! [cmap]: https://docs.microsoft.com/en-us/typography/opentype/spec/cmap
 
+use std::collections::HashSet;
+
 include!("../../generated/generated_cmap.rs");
+
+// https://learn.microsoft.com/en-us/typography/opentype/spec/cmap#unicode-platform-platform-id--0
+const UNICODE_BMP_ENCODING: u16 = 3;
+
+fn size_of_cmap4(seg_count: u16, gid_count: u16) -> u16 {
+    8 * 2  // 8 uint16's
+    + 2 * seg_count * 4  // 4 parallel arrays of len seg_count, 2 bytes per entry
+    + 2 * gid_count // 2 bytes per gid in glyphIdArray
+}
+
+impl CmapSubtable {
+    fn create_format_4(
+        lang: u16,
+        end_code: Vec<u16>,
+        start_code: Vec<u16>,
+        id_deltas: Vec<i16>,
+    ) -> Self {
+        if HashSet::from([start_code.len(), end_code.len(), id_deltas.len()]).len() != 1 {
+            panic!("uneven parallel arrays, very bad. Very very bad.");
+        }
+
+        let seg_count: u16 = start_code.len().try_into().unwrap();
+        // Spec: Log2 of the maximum power of 2 less than or equal to segCount (log2(searchRange/2),
+        // which is equal to floor(log2(segCount)))
+        let entry_selector = (seg_count as f32).log2().floor();
+
+        // Spec: Maximum power of 2 less than or equal to segCount, times 2
+        // ((2**floor(log2(segCount))) * 2, where “**” is an exponentiation operator)
+        let search_range = 2u16.pow(entry_selector as u32).checked_mul(2).unwrap();
+
+        // if 2^entry_selector*2 is a u16 then so is entry_selector
+        let entry_selector: u16 = entry_selector as u16;
+        let range_shift: u16 = seg_count * 2 - search_range;
+
+        let id_range_offsets = id_deltas.iter().map(|_| 0).collect();
+        CmapSubtable::Format4(Cmap4::new(
+            size_of_cmap4(seg_count, 0),
+            lang,
+            seg_count * 2,
+            search_range,
+            entry_selector,
+            range_shift,
+            end_code,
+            start_code,
+            id_deltas,
+            id_range_offsets,
+            vec![], // becauseour idRangeOffset's are 0 glyphIdArray is unused
+        ))
+    }
+}
+
+impl Cmap {
+    /// Generates a [cmap](https://learn.microsoft.com/en-us/typography/opentype/spec/cmap) that is expected to work in most modern environments.
+    ///
+    /// For the time being just emits [format 4](https://learn.microsoft.com/en-us/typography/opentype/spec/cmap#format-4-segment-mapping-to-delta-values)
+    /// so we can drive towards compiling working fonts. In time we may wish to additionally emit format 12 to support
+    /// novel codepoints.
+    pub fn from_mappings(mappings: impl IntoIterator<Item = (char, GlyphId)>) -> Cmap {
+        let mut end_code: Vec<u16> = Vec::new();
+        let mut start_code: Vec<u16> = Vec::new();
+        let mut id_deltas: Vec<i16> = Vec::new();
+        let mut id_range_offsets: Vec<u16> = Vec::new();
+
+        let mut prev = (u16::MAX - 1, u16::MAX - 1);
+        for (cp, gid) in mappings.into_iter() {
+            let cp = (cp as u32).try_into().unwrap();
+            let next_in_run = (
+                prev.0.checked_add(1).unwrap(),
+                prev.1.checked_add(1).unwrap(),
+            );
+            let current = (cp, gid.to_u16());
+            // Codepoint and gid need to be continuous
+            if current != next_in_run {
+                // Start a new run
+                start_code.push(cp);
+                end_code.push(cp);
+
+                // we might need to reach further than an i16 can take us
+                // using idRangeOffset ... but we're saving that for another day
+                id_deltas.push((gid.to_u16() as i32 - cp as i32).try_into().unwrap());
+                id_range_offsets.push(0);
+            } else {
+                // Continue the prior run
+                let last = end_code.last_mut().unwrap();
+                *last = cp;
+            }
+            prev = current;
+        }
+
+        // close out
+        start_code.push(0xFFFF);
+        end_code.push(0xFFFF);
+        id_deltas.push(1);
+        id_range_offsets.push(0);
+
+        Cmap::new(vec![EncodingRecord::new(
+            PlatformId::Unicode,
+            UNICODE_BMP_ENCODING,
+            CmapSubtable::create_format_4(
+                0, // set to zero for all 'cmap' subtables whose platform IDs are other than Macintosh
+                end_code, start_code, id_deltas,
+            ),
+        )])
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use font_types::{BigEndian, Scalar};
+    use font_types::{BigEndian, GlyphId, Scalar};
     use read::{
         tables::cmap::{Cmap, CmapSubtable, PlatformId},
         FontData, FontRead,
     };
 
-    use crate::{dump_table, tables::cmap as write};
-
-    // https://learn.microsoft.com/en-us/typography/opentype/spec/cmap#unicode-platform-platform-id--0
-    const UNICODE_BMP_ENCODING: u16 = 3;
-
-    // I feel like i'm doing something wrong working this out this way...
-    fn size_of_cmap4(seg_count: u16, gid_count: u16) -> u16 {
-        8 * 2  // 8 uint16's
-        + 2 * seg_count * 4  // 4 parallel arrays of len seg_count, 2 bytes per entry
-        + 2 * gid_count // 2 bytes per gid in glyphIdArray
-    }
+    use crate::{
+        dump_table,
+        tables::cmap::{self as write, UNICODE_BMP_ENCODING},
+    };
 
     fn to_vec<T: Scalar>(bees: &[BigEndian<T>]) -> Vec<T> {
         bees.iter().map(|be| be.get()).collect()
     }
 
     // https://learn.microsoft.com/en-us/typography/opentype/spec/cmap#format-4-segment-mapping-to-delta-values
+    // "map characters 10-20, 30-90, and 153-480 onto a contiguous range of glyph indices"
     #[test]
     fn generate_simple_cmap4() {
-        // building the example given in the spec
-        let seg_count: u16 = 4;
-        let search_range: u16 = 8;
-        let entry_selector: u16 = 4;
-        let range_shift: u16 = 0;
-        let end_code = vec![20u16, 90u16, 480u16, 0xffffu16];
-        let start_code = vec![10u16, 30u16, 153u16, 0xffffu16];
-        let id_delta = vec![-9i16, -18i16, -80i16, 1i16];
-        let id_range_offsets = vec![0u16, 0u16, 0u16, 0u16];
-
-        let cmap = write::Cmap::new(vec![write::EncodingRecord::new(
-            PlatformId::Unicode,
-            UNICODE_BMP_ENCODING,
-            write::CmapSubtable::Format4(write::Cmap4::new(
-                size_of_cmap4(seg_count, 0),
-                0, // The language field must be set to zero for all 'cmap' subtables whose platform IDs are other than Macintosh
-                seg_count * 2,
-                search_range,
-                entry_selector,
-                range_shift,
-                end_code.clone(),
-                start_code.clone(),
-                id_delta.clone(),
-                id_range_offsets.clone(),
-                vec![], // becauseour idRangeOffset's are 0 glyphIdArray is unused
-            )),
-        )]);
+        let mappings = (10..=20)
+            .chain(30..=90)
+            .chain(153..=480)
+            .enumerate()
+            .map(|(idx, codepoint)| (char::from_u32(codepoint).unwrap(), GlyphId::new(idx as u16)));
+        let cmap = write::Cmap::from_mappings(mappings);
 
         let bytes = dump_table(&cmap).unwrap();
         let font_data = FontData::new(&bytes);
@@ -74,9 +154,29 @@ mod tests {
             panic!("Expected a cmap4 in {encoding_record:?}");
         };
 
-        assert_eq!(start_code, to_vec(cmap4.start_code()));
-        assert_eq!(end_code, to_vec(cmap4.end_code()));
-        assert_eq!(id_delta, to_vec(cmap4.id_delta()));
-        assert_eq!(id_range_offsets, to_vec(cmap4.id_range_offsets()));
+        // The spec example says entry_selector 4 but the calculation it gives seems to yield 2 (?)
+        assert_eq!(
+            (8, 8, 2, 0),
+            (
+                cmap4.seg_count_x2(),
+                cmap4.search_range(),
+                cmap4.entry_selector(),
+                cmap4.range_shift()
+            )
+        );
+        assert_eq!(
+            vec![10u16, 30u16, 153u16, 0xffffu16],
+            to_vec(cmap4.start_code())
+        );
+        assert_eq!(
+            vec![20u16, 90u16, 480u16, 0xffffu16],
+            to_vec(cmap4.end_code())
+        );
+        // The example starts at gid 1, we're starting at 0
+        assert_eq!(vec![-10i16, -19i16, -81i16, 1i16], to_vec(cmap4.id_delta()));
+        assert_eq!(
+            vec![0u16, 0u16, 0u16, 0u16],
+            to_vec(cmap4.id_range_offsets())
+        );
     }
 }
