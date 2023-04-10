e25e97fc29d3bc6dad61055a372c44256254a20e
Rod S <rsheeter@google.com>
2023-04-10 11:40:35 -0700

    No more than 64 zeros please, we use 6 bits to encode run length

--- write-fonts/src/tables/variations.rs
+++ write-fonts/src/tables/variations.rs
@@ -119,7 +119,7 @@ impl PackedDeltas {
         fn count_leading_zeros(slice: &[i16]) -> u8 {
             slice
                 .iter()
-                .take(u8::MAX.into())
+                .take(MAX_POINTS_PER_RUN)
                 .take_while(|v| **v == 0)
                 .count() as u8
         }
@@ -491,10 +491,10 @@ mod tests {
 
     #[test]
     fn lots_of_zero() {
-        let num_zeroes = u8::MAX as usize + 1;
+        let num_zeroes = 65;
         let deltas = PackedDeltas::new(vec![0; num_zeroes]);
         assert_eq!(
-            vec![PackedDeltaRun::Zeros(u8::MAX), PackedDeltaRun::Zeros(1)],
+            vec![PackedDeltaRun::Zeros(64), PackedDeltaRun::Zeros(1)],
             deltas.iter_runs().collect::<Vec<_>>()
         );
     }
