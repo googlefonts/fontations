0309436724200ba803e72886c9e49db667e7d948
Rod S <rsheeter@google.com>
2023-04-14 20:03:22 -0700

    hangs

--- write-fonts/src/tables/avar.rs
+++ write-fonts/src/tables/avar.rs
@@ -1,3 +1,22 @@
 //! The [avar](https://learn.microsoft.com/en-us/typography/opentype/spec/avar) table
 
 include!("../../generated/generated_avar.rs");
+
+
+#[cfg(test)]
+mod tests {
+    use font_types::MajorMinor;
+    use read_fonts::{FontRead, FontData};
+    use crate::tables::avar::{Avar, SegmentMaps};
+
+    use crate::dump_table;
+
+    #[test]
+    fn hangs() {
+        let avar = Avar::new(
+            MajorMinor::VERSION_1_0,
+            vec![SegmentMaps::new(Vec::new())]);
+        let bytes = dump_table(&avar).unwrap();
+        Avar::read(FontData::new(&bytes)).unwrap();
+    }
+}
