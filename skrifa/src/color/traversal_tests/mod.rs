656f73e106c5a5c1fdead3a4251b228e0d5353dd
Dominik Röttsches <drott@chromium.org>
2023-12-15 17:10:04 +0200

    [COLRv1] Add tests for coincident stops and angles
    Adjust PaintSweepGradient implementation to not reverse color stops when
    not needed. Matches FireFox behavior. See Skia change under review for
    implementing the same change [1].
    
    [1] https://skia-review.googlesource.com/c/skia/+/790456

--- skrifa/src/color/traversal_tests/mod.rs
+++ skrifa/src/color/traversal_tests/mod.rs
@@ -355,4 +355,5 @@ variable_alpha_default:VARIABLE_ALPHA,&[],
 variable_alpha_var1:VARIABLE_ALPHA,&[("APH1", -0.7)],
 variable_alpha_var2:VARIABLE_ALPHA,&[("APH2", -0.7), ("APH3", -0.2)],
 nocycle_multi_colrglyph:NO_CYCLE_MULTI_COLRGLYPH,&[],
+sweep_coincident:SWEEP_COINCIDENT,&[],
 );
