cc1ef7b46b07cb285024ac5b14602fd016380d13
Chad Brokaw <cbrokaw@gmail.com>
2024-03-28 21:40:16 -0400

    remove Option from data_size field
    all formats can compute a data size

--- read-fonts/src/tables/bitmap.rs
+++ read-fonts/src/tables/bitmap.rs
@@ -47,12 +47,12 @@ impl BitmapSize {
                     if end < start {
                         return Err(ReadError::OutOfBounds);
                     }
-                    location.data_size = Some(end - start);
+                    location.data_size = end - start;
                 }
                 IndexSubtable::Format2(st) => {
                     location.format = st.image_format();
                     let data_size = st.image_size() as usize;
-                    location.data_size = Some(data_size);
+                    location.data_size = data_size;
                     location.data_offset = st.image_data_offset() as usize + glyph_ix * data_size;
                     location.metrics = Some(st.big_metrics()[0]);
                 }
@@ -72,7 +72,7 @@ impl BitmapSize {
                     if end < start {
                         return Err(ReadError::OutOfBounds);
                     }
-                    location.data_size = Some(end - start);
+                    location.data_size = end - start;
                 }
                 IndexSubtable::Format4(st) => {
                     location.format = st.image_format();
@@ -92,7 +92,7 @@ impl BitmapSize {
                     if end < start {
                         return Err(ReadError::OutOfBounds);
                     }
-                    location.data_size = Some(end - start);
+                    location.data_size = end - start;
                 }
                 IndexSubtable::Format5(st) => {
                     location.format = st.image_format();
@@ -104,7 +104,7 @@ impl BitmapSize {
                         }
                     };
                     let data_size = st.image_size() as usize;
-                    location.data_size = Some(data_size);
+                    location.data_size = data_size;
                     location.data_offset = st.image_data_offset() as usize + array_ix * data_size;
                     location.metrics = Some(st.big_metrics()[0]);
                 }
@@ -151,13 +151,21 @@ pub struct BitmapLocation {
     pub format: u16,
     /// Offset in bytes from the start of the EBDT/CBDT table.
     pub data_offset: usize,
-    /// Size of the image data in bytes, if present in the EBLC/CBLC table.
-    pub data_size: Option<usize>,
+    /// Size of the image data in bytes.
+    pub data_size: usize,
     /// Bit depth from the associated size. Required for computing image data
     /// size when unspecified.
     pub bit_depth: u8,
     /// Full metrics, if present in the EBLC/CBLC table.
     pub metrics: Option<BigGlyphMetrics>,
+}
+
+impl BitmapLocation {
+    /// Returns true if the location references an empty bitmap glyph such as
+    /// a space.
+    pub fn is_empty(&self) -> bool {
+        self.data_size == 0
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -192,12 +200,8 @@ pub(crate) fn bitmap_data<'a>(
     location: &BitmapLocation,
     is_color: bool,
 ) -> Result<BitmapData<'a>, ReadError> {
-    let end = location
-        .data_size
-        .map(|size| size + location.data_offset)
-        .unwrap_or(offset_data.len());
     let mut image_data = offset_data
-        .slice(location.data_offset..end)
+        .slice(location.data_offset..location.data_offset + location.data_size)
         .ok_or(ReadError::OutOfBounds)?
         .cursor();
     match location.format {
