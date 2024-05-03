6c6e543bb2dbdf85af61686ccf7928108c87623d
Garret Rieger <grieger@google.com>
2024-05-03 21:59:46 +0000

    Implement FromIterator, PartialEq, and Hash on IntSet.

--- int-set/src/lib.rs
+++ int-set/src/lib.rs
@@ -20,6 +20,11 @@ enum Membership<T> {
 impl<T: Into<u32> + Copy + Default> IntSet<T> {
     /// Create a new empty set.
     pub fn default() -> IntSet<T> {
+        IntSet::<T>::empty()
+    }
+
+    /// Create a new empty set.
+    pub fn empty() -> IntSet<T> {
         IntSet::<T>(Membership::Inclusive(BitSet::<T>::default()))
     }
 
@@ -100,8 +105,53 @@ impl<T> IntSet<T> {
     }
 }
 
+impl<T> std::hash::Hash for IntSet<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        match &self.0 {
+            Membership::<T>::Inclusive(s) => {
+                s.hash(state);
+                0u32.hash(state);
+            }
+            Membership::<T>::Exclusive(s) => {
+                s.hash(state);
+                1u32.hash(state);
+            }
+        }
+    }
+}
+
+impl<T> std::cmp::Eq for IntSet<T> {}
+
+impl<T> std::cmp::PartialEq for IntSet<T> {
+    fn eq(&self, other: &Self) -> bool {
+        match (&self.0, &other.0) {
+            (Membership::<T>::Inclusive(a), Membership::<T>::Inclusive(b)) => a == b,
+            (Membership::<T>::Exclusive(a), Membership::<T>::Exclusive(b)) => a == b,
+            _ => return false,
+        }
+    }
+}
+
+impl<T: Into<u32> + Copy + Default> FromIterator<T> for IntSet<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        // TODO(garretrieger): implement a more efficient version of this which avoids page lookups
+        //  when the iterator values are in sorted order (eg. if the next value is on the same page as
+        //  the previous value). This will require BitSet to also implement FromIterator.
+        let mut s = IntSet::<T>::empty();
+        for i in iter {
+            s.insert(i);
+        }
+        s
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::{
+        collections::HashSet,
+        hash::{DefaultHasher, Hash, Hasher},
+    };
+
     use super::*;
 
     #[test]
@@ -137,6 +187,53 @@ mod test {
         assert!(set.is_empty());
         let set_inverted = set_inverted.clear();
         assert!(set_inverted.is_empty());
+    }
+
+    #[test]
+    fn equal_an_hash() {
+        let mut inc1 = IntSet::<u32>::empty();
+        inc1.insert(14);
+        inc1.insert(670);
+
+        let mut inc2 = IntSet::<u32>::empty();
+        inc2.insert(670);
+        inc2.insert(14);
+
+        let mut inc3 = inc1.clone();
+        inc3.insert(5);
+
+        let exc = inc1.clone().inverted();
+
+        assert_eq!(inc1, inc2);
+        assert_ne!(inc1, inc3);
+        assert_ne!(inc1, exc);
+
+        let set = HashSet::from([inc1.clone(), inc3.clone(), exc.clone()]);
+        assert!(set.contains(&inc1));
+        assert!(set.contains(&inc3));
+        assert!(set.contains(&exc));
+
+        let mut h1 = DefaultHasher::new();
+        let mut h2 = DefaultHasher::new();
+        let mut h3 = DefaultHasher::new();
+        inc1.hash(&mut h1);
+        exc.hash(&mut h2);
+        inc2.hash(&mut h3);
+
+        assert_ne!(h1.finish(), h2.finish());
+        assert_eq!(h1.finish(), h3.finish());
+    }
+
+    #[test]
+    fn from_iterator() {
+        let s: IntSet<u32> = vec![3, 8, 12, 589].iter().copied().collect();
+        let mut expected = IntSet::<u32>::empty();
+        expected.insert(3);
+        expected.insert(8);
+        expected.insert(12);
+        expected.insert(589);
+
+        assert_eq!(s, expected);
     }
 
     #[test]
