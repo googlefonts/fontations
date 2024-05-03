9dc1da179923e64c4ea065675d0d6208e7f04e6d
Garret Rieger <grieger@google.com>
2024-05-03 18:43:57 +0000

    Wrap the Standard/Inverted enum in a struct to avoid BitSet visibility issues.
    Also rename the enum to Membership::Inclusive/Exclusive to be more clear about what it's doing.

--- int-set/src/lib.rs
+++ int-set/src/lib.rs
@@ -6,78 +6,81 @@ use std::ops::RangeInclusive;
 
 /// A fast & efficient unsigned integer (u32) bit set which is invertible.
 #[derive(Clone, Debug)]
-pub enum IntSet<T> {
+pub struct IntSet<T>(Membership<T>);
+
+#[derive(Clone, Debug)]
+enum Membership<T> {
     /// Records a set of integers which are members of the set.
-    Standard(BitSet<T>),
+    Inclusive(BitSet<T>),
 
     /// Records the set of integers which are not members of the set.
-    Inverted(BitSet<T>),
+    Exclusive(BitSet<T>),
 }
 
 impl<T: Into<u32> + Copy + Default> IntSet<T> {
     /// Create a new empty set.
     pub fn default() -> IntSet<T> {
-        IntSet::Standard(BitSet::<T>::default())
+        IntSet::<T>(Membership::Inclusive(BitSet::<T>::default()))
     }
 
     /// Create a new set which contains all integers.
     pub fn all() -> IntSet<T> {
-        IntSet::Inverted(BitSet::<T>::default())
+        IntSet::<T>(Membership::Exclusive(BitSet::<T>::default()))
     }
 
     /// Return the inverted version of this set.
     pub fn inverted(self) -> IntSet<T> {
-        match self {
-            IntSet::<T>::Standard(s) => IntSet::<T>::Inverted(s),
-            IntSet::<T>::Inverted(s) => IntSet::<T>::Standard(s),
+        match self.0 {
+            Membership::<T>::Inclusive(s) => IntSet::<T>(Membership::<T>::Exclusive(s)),
+            Membership::<T>::Exclusive(s) => IntSet::<T>(Membership::<T>::Inclusive(s)),
         }
     }
 
     /// Return a new version of this set with all members removed.
     pub fn clear(mut self) -> IntSet<T> {
         self.clear_internal_set();
-        match self {
-            IntSet::<T>::Standard(s) => IntSet::<T>::Standard(s),
-            IntSet::<T>::Inverted(s) => IntSet::<T>::Standard(s),
+        match self.0 {
+            Membership::<T>::Inclusive(s) => IntSet::<T>(Membership::<T>::Inclusive(s)),
+            Membership::<T>::Exclusive(s) => IntSet::<T>(Membership::<T>::Inclusive(s)),
         }
     }
 
     /// Add val as a member of this set.
     pub fn insert(&mut self, val: T) -> bool {
-        match self {
-            IntSet::<T>::Standard(s) => s.insert(val),
-            IntSet::<T>::Inverted(s) => s.remove(val),
+        match &mut self.0 {
+            Membership::<T>::Inclusive(s) => s.insert(val),
+            Membership::<T>::Exclusive(s) => s.remove(val),
         }
     }
 
     /// Add all values in range as members of this set.
     pub fn insert_range(&mut self, range: RangeInclusive<T>) {
-        match self {
-            IntSet::<T>::Standard(s) => s.insert_range(range),
-            IntSet::<T>::Inverted(_) => todo!("implement bitset::remove_range and call here."),
+        match &mut self.0 {
+            Membership::<T>::Inclusive(s) => s.insert_range(range),
+            Membership::<T>::Exclusive(_) => todo!("implement bitset::remove_range and call here."),
         }
     }
 
     /// Remove val from this set.
     pub fn remove(&mut self, val: T) -> bool {
-        match self {
-            IntSet::<T>::Standard(s) => s.remove(val),
-            IntSet::<T>::Inverted(s) => s.insert(val),
+        match &mut self.0 {
+            Membership::<T>::Inclusive(s) => s.remove(val),
+            Membership::<T>::Exclusive(s) => s.insert(val),
         }
     }
 
     /// Returns true if val is a member of this set.
     pub fn contains(&self, val: T) -> bool {
-        match self {
-            IntSet::<T>::Standard(s) => s.contains(val),
-            IntSet::<T>::Inverted(s) => !s.contains(val),
+        match &self.0 {
+            Membership::<T>::Inclusive(s) => s.contains(val),
+            Membership::<T>::Exclusive(s) => !s.contains(val),
         }
     }
 
     fn clear_internal_set(&mut self) {
-        match self {
-            IntSet::<T>::Standard(s) => s.clear(),
-            IntSet::<T>::Inverted(s) => s.clear(),
+        match &mut self.0 {
+            Membership::<T>::Inclusive(s) => s.clear(),
+            Membership::<T>::Exclusive(s) => s.clear(),
         }
     }
 }
@@ -85,9 +88,9 @@ impl<T: Into<u32> + Copy + Default> IntSet<T> {
 impl<T> IntSet<T> {
     /// Returns the number of members in this set.
     pub fn len(&self) -> usize {
-        match self {
-            IntSet::<T>::Standard(s) => s.len(),
-            IntSet::<T>::Inverted(s) => u32::MAX as usize - s.len(),
+        match &self.0 {
+            Membership::<T>::Inclusive(s) => s.len(),
+            Membership::<T>::Exclusive(s) => u32::MAX as usize - s.len(),
         }
     }
 
