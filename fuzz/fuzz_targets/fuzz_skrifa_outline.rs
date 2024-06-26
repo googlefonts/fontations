9ca084e6135b33f21a3ff6a0305620170060d820
rsheeter <rsheeter@google.com>
2024-06-25 19:00:22 -0700

    Try to play nicer with the seed corpus

--- fuzz/fuzz_targets/fuzz_skrifa_outline.rs
+++ fuzz/fuzz_targets/fuzz_skrifa_outline.rs
@@ -1,10 +1,9 @@
 #![no_main]
 use std::{error::Error, fmt::Display};
 
-use libfuzzer_sys::{
-    arbitrary::{self, Arbitrary, Unstructured},
-    fuzz_target,
-};
+use libfuzzer_sys::fuzz_target;
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
 use skrifa::{
     instance::Size,
     outline::{DrawError, DrawSettings, HintingInstance, HintingMode, LcdLayout, OutlinePen},
@@ -37,50 +36,19 @@ impl OutlinePen for NopPen {
     }
 }
 
-/// Each entry represents a set of options for [HintingMode]
-///
-/// Exists to fulfill [Arbitrary]
-#[derive(Arbitrary, Debug)]
-enum FuzzerHintingMode {
-    Strong,
-    Smooth {
-        vertical_lcd: Option<bool>,
-        preserve_linear_metrics: bool,
-    },
-}
-
-impl From<FuzzerHintingMode> for HintingMode {
-    fn from(value: FuzzerHintingMode) -> Self {
-        match value {
-            FuzzerHintingMode::Strong => HintingMode::Strong,
-            FuzzerHintingMode::Smooth {
-                vertical_lcd,
-                preserve_linear_metrics,
-            } => HintingMode::Smooth {
-                lcd_subpixel: match vertical_lcd {
-                    None => None,
-                    Some(true) => Some(LcdLayout::Vertical),
-                    Some(false) => Some(LcdLayout::Horizontal),
-                },
-                preserve_linear_metrics,
-            },
-        }
-    }
-}
-
 /// Drawing glyph outlines is fun and flexible! Try to test lots of options.
 ///
 /// See
 /// * <https://rust-fuzz.github.io/book/cargo-fuzz/structure-aware-fuzzing.html>
 /// * <https://docs.rs/skrifa/latest/skrifa/outline/index.html>
-#[derive(Arbitrary, Debug)]
+#[derive(Default, Debug, Clone)]
 struct OutlineRequest {
     /// None => unscaled
     size: Option<f32>,
     axis_positions: Vec<f32>,
     hinted: bool,
     hinted_pedantic: bool,
-    hinting_mode: FuzzerHintingMode,
+    hinting_mode: HintingMode,
     with_memory: bool, // ~half tests should be with memory, half not
     memory_size: u16, // if we do test with_memory, how much of it? u16 to avoid asking for huge chunks.
     harfbuzz_pathstyle: bool,
@@ -119,13 +87,8 @@ fn do_glyf_things(outline_request: OutlineRequest, data: &[u8]) -> Result<(), Bo
     );
     let hinting_instance = if outline_request.hinted {
         Some(
-            HintingInstance::new(
-                &outlines,
-                size,
-                &location,
-                outline_request.hinting_mode.into(),
-            )
-            .map_err(DrawErrorWrapper)?,
+            HintingInstance::new(&outlines, size, &location, outline_request.hinting_mode)
+                .map_err(DrawErrorWrapper)?,
         )
     } else {
         None
@@ -154,11 +117,115 @@ fn do_glyf_things(outline_request: OutlineRequest, data: &[u8]) -> Result<(), Bo
     Ok(())
 }
 
+fn create_rng(data: &[u8]) -> ChaCha8Rng {
+    let mut seed = [0u8; 32];
+    for (i, entry) in seed.iter_mut().enumerate() {
+        *entry = data.get(i).copied().unwrap_or_default();
+    }
+    ChaCha8Rng::from_seed(seed)
+}
+
+fn hinting_modes(hinted: bool) -> Vec<HintingMode> {
+    if !hinted {
+        return vec![HintingMode::default()];
+    }
+    vec![
+        HintingMode::Strong,
+        HintingMode::Smooth {
+            lcd_subpixel: None,
+            preserve_linear_metrics: true,
+        },
+        HintingMode::Smooth {
+            lcd_subpixel: None,
+            preserve_linear_metrics: false,
+        },
+        HintingMode::Smooth {
+            lcd_subpixel: Some(LcdLayout::Horizontal),
+            preserve_linear_metrics: true,
+        },
+        HintingMode::Smooth {
+            lcd_subpixel: Some(LcdLayout::Horizontal),
+            preserve_linear_metrics: false,
+        },
+        HintingMode::Smooth {
+            lcd_subpixel: Some(LcdLayout::Vertical),
+            preserve_linear_metrics: true,
+        },
+        HintingMode::Smooth {
+            lcd_subpixel: Some(LcdLayout::Vertical),
+            preserve_linear_metrics: false,
+        },
+    ]
+}
+
 fuzz_target!(|data: &[u8]| {
-    let mut unstructured = Unstructured::new(data);
-    let Ok(outline_request) = unstructured.arbitrary() else {
-        return;
-    };
-    let data = unstructured.take_rest();
-    let _ = do_glyf_things(outline_request, data);
+    // data from corpus is likely to be a font. If we chope off the head to make an outline request
+    // it is likely data is no longer a font. So, take the cross product of likely values for various options
+    // If a lot of values are possible choose randomly with rng seeded from data to ensure reproducible results.
+    let mut rng = create_rng(data);
+    let random_position = (0..8)
+        .map(|_| rng.gen_range(-1000.0..1000.0))
+        .collect::<Vec<_>>();
+    let memory_sizes = vec![4096, 16384, rng.gen::<u16>(), rng.gen::<u16>()];
+
+    let mut requests = vec![OutlineRequest::default()];
+    requests = requests
+        .into_iter()
+        .flat_map(|r| {
+            [None, Some(64.0), Some(512.0)]
+                .into_iter()
+                .map(move |size| OutlineRequest { size, ..r.clone() })
+        })
+        .flat_map(|r| {
+            vec![
+                // default
+                (0..8).map(|_| 0.0).collect(),
+                // random
+                random_position.clone(),
+            ]
+            .into_iter()
+            .map(move |axis_positions| OutlineRequest {
+                axis_positions,
+                ..r.clone()
+            })
+        })
+        .flat_map(|r| {
+            [true, false].into_iter().map(move |hinted| OutlineRequest {
+                hinted,
+                ..r.clone()
+            })
+        })
+        .flat_map(|r| {
+            hinting_modes(r.hinted)
+                .into_iter()
+                .map(move |hinting_mode| OutlineRequest {
+                    hinting_mode,
+                    ..r.clone()
+                })
+        })
+        .flat_map(|r| {
+            [true, false]
+                .into_iter()
+                .map(move |with_memory| OutlineRequest {
+                    with_memory,
+                    ..r.clone()
+                })
+        })
+        .flat_map(|r| {
+            if r.with_memory {
+                memory_sizes.clone()
+            } else {
+                vec![0]
+            }
+            .into_iter()
+            .map(move |memory_size| OutlineRequest {
+                memory_size,
+                ..r.clone()
+            })
+        })
+        .collect();
+
+    for request in requests {
+        let _ = do_glyf_things(request, data);
+    }
 });
