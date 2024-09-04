72e67773825bf3e6a59ca730956cc1394487efec
Chad Brokaw <cbrokaw@gmail.com>
2024-09-03 23:50:32 -0400

    [skrifa] autohint: scale CJK metrics
    Computes scaled style metrics for CJK autohinting.

--- skrifa/src/outline/autohint/latin/metrics.rs
+++ skrifa/src/outline/autohint/latin/metrics.rs
@@ -13,9 +13,9 @@ use super::super::{
         ScaledStyleMetrics, ScaledWidth, UnscaledAxisMetrics, UnscaledBlue, UnscaledBlues,
         UnscaledStyleMetrics, WidthMetrics,
     },
-    style::{blue_flags, ScriptClass, StyleClass},
+    style::{blue_flags, ScriptClass, ScriptGroup, StyleClass},
 };
-use crate::{prelude::Size, MetadataProvider};
+use crate::{outline::autohint::metrics::fixed_div, prelude::Size, MetadataProvider};
 use raw::{types::F2Dot14, FontRef};
 
 /// Computes unscaled metrics for the Latin writing system.
@@ -71,8 +71,13 @@ pub(crate) fn scale_style_metrics(
     unscaled_metrics: &UnscaledStyleMetrics,
     mut scale: Scale,
 ) -> ScaledStyleMetrics {
+    let scale_axis_fn = if unscaled_metrics.style_class().script.group == ScriptGroup::Default {
+        scale_default_axis_metrics
+    } else {
+        scale_cjk_axis_metrics
+    };
     let mut scale_axis = |axis: &UnscaledAxisMetrics| {
-        scale_axis_metrics(
+        scale_axis_fn(
             axis.dim,
             &axis.widths,
             axis.width_metrics,
@@ -90,7 +95,7 @@ pub(crate) fn scale_style_metrics(
 /// Computes scaled metrics for a single axis.
 ///
 /// See <https://gitlab.freedesktop.org/freetype/freetype/-/blob/57617782464411201ce7bbc93b086c1b4d7d84a5/src/autofit/aflatin.c#L1168>
-fn scale_axis_metrics(
+fn scale_default_axis_metrics(
     dim: Dimension,
     widths: &[i32],
     width_metrics: WidthMetrics,
@@ -155,7 +160,7 @@ fn scale_axis_metrics(
                     scaled: scaled_overshoot,
                     fitted: scaled_overshoot,
                 },
-                flags: unscaled_blue.flags & !blue_flags::LATIN_ACTIVE,
+                flags: unscaled_blue.flags & !blue_flags::ACTIVE,
             };
             // Only activate blue zones less than 3/4 pixel tall
             let dist = fixed_mul(unscaled_blue.position - unscaled_blue.overshoot, axis.scale);
@@ -173,14 +178,14 @@ fn scale_axis_metrics(
                 }
                 blue.position.fitted = pix_round(blue.position.scaled);
                 blue.overshoot.fitted = blue.position.fitted - delta;
-                blue.flags |= blue_flags::LATIN_ACTIVE;
+                blue.flags |= blue_flags::ACTIVE;
             }
             axis.blues.push(blue);
         }
         // Use sub-top blue zone if it doesn't overlap with another
         // non-sub-top blue zone
         for blue_ix in 0..axis.blues.len() {
-            const REQUIRED_FLAGS: u32 = blue_flags::LATIN_SUB_TOP | blue_flags::LATIN_ACTIVE;
+            const REQUIRED_FLAGS: u32 = blue_flags::LATIN_SUB_TOP | blue_flags::ACTIVE;
             let blue = axis.blues[blue_ix];
             if blue.flags & REQUIRED_FLAGS != REQUIRED_FLAGS {
                 continue;
@@ -190,17 +195,80 @@ fn scale_axis_metrics(
                 if blue2.flags & blue_flags::LATIN_SUB_TOP != 0 {
                     continue;
                 }
-                if blue2.flags & blue_flags::LATIN_ACTIVE == 0 {
+                if blue2.flags & blue_flags::ACTIVE == 0 {
                     continue;
                 }
                 if blue2.position.fitted <= blue.overshoot.fitted
                     && blue2.overshoot.fitted >= blue.position.fitted
                 {
-                    axis.blues[blue_ix].flags &= !blue_flags::LATIN_ACTIVE;
+                    axis.blues[blue_ix].flags &= !blue_flags::ACTIVE;
                     break;
                 }
             }
         }
+    }
+    axis
+}
+
+/// Computes scaled metrics for a single axis for the CJK script gorup.
+///
+/// See <https://gitlab.freedesktop.org/freetype/freetype/-/blob/57617782464411201ce7bbc93b086c1b4d7d84a5/src/autofit/afcjk.c#L661>
+fn scale_cjk_axis_metrics(
+    dim: Dimension,
+    widths: &[i32],
+    width_metrics: WidthMetrics,
+    blues: &[UnscaledBlue],
+    scale: &mut Scale,
+) -> ScaledAxisMetrics {
+    let mut axis = ScaledAxisMetrics::default();
+    if dim == Axis::HORIZONTAL {
+        axis.scale = scale.x_scale;
+        axis.delta = scale.x_delta;
+    } else {
+        axis.scale = scale.y_scale;
+        axis.delta = scale.y_delta;
+    };
+    let scale = axis.scale;
+    // Scale the blue zones
+    for unscaled_blue in blues {
+        let position = fixed_mul(unscaled_blue.position, scale) + axis.delta;
+        let overshoot = fixed_mul(unscaled_blue.overshoot, scale) + axis.delta;
+        let mut blue = ScaledBlue {
+            position: ScaledWidth {
+                scaled: position,
+                fitted: position,
+            },
+            overshoot: ScaledWidth {
+                scaled: overshoot,
+                fitted: overshoot,
+            },
+            flags: unscaled_blue.flags,
+        };
+        // A blue zone is only active if it is less than 3/4 pixels tall
+        let dist = fixed_mul(unscaled_blue.position - unscaled_blue.overshoot, scale);
+        if dist <= 48 || dist >= -48 {
+            blue.position.fitted = pix_round(blue.position.scaled);
+            // For CJK, "overshoot" is actually undershoot
+            let delta1 = fixed_div(blue.position.fitted, scale) - unscaled_blue.overshoot;
+            let mut delta2 = fixed_mul(delta1.abs(), scale);
+            if delta2 < 32 {
+                delta2 = 0;
+            } else {
+                delta2 = pix_round(delta2);
+            }
+            if delta1 < 0 {
+                delta2 = -delta2;
+            }
+            blue.overshoot.fitted = blue.position.fitted - delta2;
+            blue.flags |= blue_flags::ACTIVE;
+        }
+        axis.blues.push(blue);
+    }
+    // FreeType never seems to compute scaled width values. We'll just
+    // match this behavior for now.
+    // <https://github.com/googlefonts/fontations/issues/1129>
+    for _ in 0..widths.len() {
+        axis.widths.push(ScaledWidth::default());
     }
     axis
 }
@@ -212,17 +280,13 @@ mod tests {
     use raw::{FontRef, TableProvider};
 
     #[test]
-    fn scaled_metrics() {
-        let font = FontRef::new(font_test_data::NOTOSERIFHEBREW_AUTOHINT_METRICS).unwrap();
-        let class = &style::STYLE_CLASSES[StyleClass::HEBR];
-        let unscaled_metrics = compute_unscaled_style_metrics(&font, Default::default(), class);
-        let scale = Scale::new(
-            16.0,
-            font.head().unwrap().units_per_em() as i32,
-            Style::Normal,
-            Default::default(),
+    fn scaled_metrics_default() {
+        // Note: expected values scraped from a FreeType debugging
+        // session
+        let scaled_metrics = make_scaled_metrics(
+            font_test_data::NOTOSERIFHEBREW_AUTOHINT_METRICS,
+            StyleClass::HEBR,
         );
-        let scaled_metrics = scale_style_metrics(&unscaled_metrics, scale);
         // Check scale and deltas
         assert_eq!(scaled_metrics.scale.x_scale, 67109);
         assert_eq!(scaled_metrics.scale.y_scale, 67109);
@@ -231,46 +295,104 @@ mod tests {
         // Horizontal widths
         let h_axis = &scaled_metrics.axes[0];
         let expected_h_widths = [55];
-        let h_widths = h_axis
-            .widths
-            .iter()
-            .map(|width| width.scaled)
-            .collect::<Vec<_>>();
-        assert_eq!(h_widths, expected_h_widths);
-        // Latin never has horizontal blues
-        assert!(h_axis.blues.is_empty());
+        // No horizontal blues
+        check_axis(h_axis, &expected_h_widths, &[]);
         // Not extra light
         assert!(!h_axis.width_metrics.is_extra_light);
         // Vertical widths
         let v_axis = &scaled_metrics.axes[1];
         let expected_v_widths = [22, 112];
-        let v_widths = v_axis
-            .widths
-            .iter()
-            .map(|width| width.scaled)
-            .collect::<Vec<_>>();
-        assert_eq!(v_widths, expected_v_widths);
         // Vertical blues
         #[rustfmt::skip]
         let expected_v_blues = [
             // ((scaled_pos, fitted_pos), (scaled_shoot, fitted_shoot), flags)
-            ((606, 576), (606, 576), blue_flags::LATIN_ACTIVE | blue_flags::TOP),
-            ((0, 0), (-9, 0), blue_flags::LATIN_ACTIVE),
-            ((-246, -256), (-246, -256), blue_flags::LATIN_ACTIVE),
+            ScaledBlue::from(((606, 576), (606, 576), blue_flags::ACTIVE | blue_flags::TOP)),
+            ScaledBlue::from(((0, 0), (-9, 0), blue_flags::ACTIVE)),
+            ScaledBlue::from(((-246, -256), (-246, -256), blue_flags::ACTIVE)),
         ];
-        let v_blues = v_axis
-            .blues
-            .iter()
-            .map(|blue| {
-                (
-                    (blue.position.scaled, blue.position.fitted),
-                    (blue.overshoot.scaled, blue.overshoot.fitted),
-                    blue.flags,
-                )
-            })
-            .collect::<Vec<_>>();
-        assert_eq!(v_blues, expected_v_blues);
+        check_axis(v_axis, &expected_v_widths, &expected_v_blues);
         // This one is extra light
         assert!(v_axis.width_metrics.is_extra_light);
+    }
+
+    #[test]
+    fn cjk_scaled_metrics() {
+        // Note: expected values scraped from a FreeType debugging
+        // session
+        let scaled_metrics = make_scaled_metrics(
+            font_test_data::NOTOSERIFTC_AUTOHINT_METRICS,
+            StyleClass::HANI,
+        );
+        // Check scale and deltas
+        assert_eq!(scaled_metrics.scale.x_scale, 67109);
+        assert_eq!(scaled_metrics.scale.y_scale, 67109);
+        assert_eq!(scaled_metrics.scale.x_delta, 0);
+        assert_eq!(scaled_metrics.scale.y_delta, 0);
+        // Horizontal widths
+        let h_axis = &scaled_metrics.axes[0];
+        let expected_h_widths = [0];
+        check_axis(h_axis, &expected_h_widths, &[]);
+        // Not extra light
+        assert!(!h_axis.width_metrics.is_extra_light);
+        // Vertical widths
+        let v_axis = &scaled_metrics.axes[1];
+        let expected_v_widths = [0];
+        // Vertical blues
+        #[rustfmt::skip]
+        let expected_v_blues = [
+            // ((scaled_pos, fitted_pos), (scaled_shoot, fitted_shoot), flags)
+            ScaledBlue::from(((857, 832), (844, 832), blue_flags::ACTIVE | blue_flags::TOP)),
+            ScaledBlue::from(((-80, -64), (-68, -64), blue_flags::ACTIVE)),
+        ];
+        // No horizontal blues
+        check_axis(v_axis, &expected_v_widths, &expected_v_blues);
+        // Also not extra light
+        assert!(!v_axis.width_metrics.is_extra_light);
+    }
+
+    fn make_scaled_metrics(font_data: &[u8], style_class: usize) -> ScaledStyleMetrics {
+        let font = FontRef::new(font_data).unwrap();
+        let class = &style::STYLE_CLASSES[style_class];
+        let unscaled_metrics = compute_unscaled_style_metrics(&font, Default::default(), class);
+        let scale = Scale::new(
+            16.0,
+            font.head().unwrap().units_per_em() as i32,
+            Style::Normal,
+            Default::default(),
+        );
+        scale_style_metrics(&unscaled_metrics, scale)
+    }
+
+    fn check_axis(
+        axis: &ScaledAxisMetrics,
+        expected_widths: &[i32],
+        expected_blues: &[ScaledBlue],
+    ) {
+        let widths = axis
+            .widths
+            .iter()
+            .map(|width| width.scaled)
+            .collect::<Vec<_>>();
+        assert_eq!(widths, expected_widths);
+        assert_eq!(axis.blues.as_slice(), expected_blues);
+    }
+
+    impl From<(i32, i32)> for ScaledWidth {
+        fn from(value: (i32, i32)) -> Self {
+            Self {
+                scaled: value.0,
+                fitted: value.1,
+            }
+        }
+    }
+
+    impl From<((i32, i32), (i32, i32), u32)> for ScaledBlue {
+        fn from(value: ((i32, i32), (i32, i32), u32)) -> Self {
+            Self {
+                position: value.0.into(),
+                overshoot: value.1.into(),
+                flags: value.2,
+            }
+        }
     }
 }
