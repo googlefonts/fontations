6e445f895fbefee5cdaecc847734a88167015466
Hoolean <me@hoolean.com>
2024-11-11 23:14:08 +0000

    Add initial implementation of `meta` table, with read and write
    This commit adds the code necessary to read and write the `meta` table,
    including its codegen definition, manual extras to handle the variable
    length data fields, and plumbing to surface it within `read-fonts` and
    `write-fonts`.
    
    Where possible, this commit borrows liberally from the implementation of
    other tables, especially `name` (for its handling of variable length
    data).
    
    This is an early implementation, and as such requires manual and
    automated testing. Additionally, there is an implementation of the
    FontWrite trait for &[u8] that may be redundant.

---
+++ resources/codegen_inputs/meta.rs
@@ -0,0 +1,34 @@
+#![parse_module(read_fonts::tables::meta)]
+
+/// [`meta`](https://docs.microsoft.com/en-us/typography/opentype/spec/meta)
+#[tag = "meta"]
+table Meta {
+    /// Version number of the metadata table — set to 1.
+    #[compile(1)]
+    version: u32,
+    /// Flags — currently unused; set to 0.
+    #[compile(0)]
+    flags: u32,
+    /// Not used; set to 0.
+    #[skip_getter]
+    #[compile(0)]
+    reserved: u32,
+    /// The number of data maps in the table.
+    data_maps_count: u32,
+    /// Array of data map records.
+    #[count($data_maps_count)]
+    data_maps: [DataMapRecord],
+}
+
+/// https://learn.microsoft.com/en-us/typography/opentype/spec/meta#table-formats
+record DataMapRecord {
+    /// A tag indicating the type of metadata.
+    tag: Tag,
+    /// Offset in bytes from the beginning of the metadata table to the data for this tag.
+    #[offset_getter(data)]
+    #[compile_with(compile_map_value)]
+    data_offset: Offset32<[u8]>,
+    /// Length of the data, in bytes. The data is not required to be padded to any byte boundary.
+    #[compile(skip)]
+    data_length: u32,
+}
