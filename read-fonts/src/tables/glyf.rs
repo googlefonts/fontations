f22b1cf728f26e33009eec0c00231990271432c6
Chad Brokaw <cbrokaw@gmail.com>
2024-12-10 13:10:25 -0500

    typo :/

--- read-fonts/src/tables/glyf.rs
+++ read-fonts/src/tables/glyf.rs
@@ -807,7 +807,7 @@ fn midpoint_i32(a: i32, b: i32) -> i32 {
     // Original overflowing code was: (a + b) / 2
     // Choose wrapping arithmetic here because we shouldn't ever
     // hit this outside of fuzzing or broken fonts _and_ this is
-    // called from very performance sensistive code
+    // called from very performance sensitive code
     a.wrapping_add(b.wrapping_sub(a) / 2)
 }
 
