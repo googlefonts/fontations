f090d3cf6b784d2755942d669efa3757a48f53d3
Garret Rieger <grieger@google.com>
2025-01-15 21:43:48 +0000

    [IFT] added test for patching gvar with long offsets.

--- incremental-font-transfer/src/glyph_keyed.rs
+++ incremental-font-transfer/src/glyph_keyed.rs
@@ -704,8 +704,9 @@ pub(crate) mod tests {
 
     use font_test_data::ift::{
         glyf_and_gvar_u16_glyph_patches, glyf_u16_glyph_patches, glyf_u16_glyph_patches_2,
-        glyph_keyed_patch_header, noop_glyf_glyph_patches, out_of_order_gvar_with_shared_tuples,
-        short_gvar_with_no_shared_tuples, short_gvar_with_shared_tuples, test_font_for_patching,
+        glyph_keyed_patch_header, long_gvar_with_shared_tuples, noop_glyf_glyph_patches,
+        out_of_order_gvar_with_shared_tuples, short_gvar_with_no_shared_tuples,
+        short_gvar_with_shared_tuples, test_font_for_patching,
         test_font_for_patching_with_loca_mod,
     };
     use skrifa::{FontRef, Tag};
@@ -1041,6 +1042,66 @@ pub(crate) mod tests {
             b'm', b'n', // gid 2
             b'o', b'p', b'q', 0, // gid 7
             b'r', 0u8, // gid 8
+        ]);
+        assert_eq!(&expected_gvar, new_gvar);
+    }
+
+    #[test]
+    fn glyph_keyed_glyf_and_long_gvar() {
+        let patch = assemble_glyph_keyed_patch(
+            glyph_keyed_patch_header(),
+            glyf_and_gvar_u16_glyph_patches(),
+        );
+        let patch: &[u8] = &patch;
+        let patch = GlyphKeyedPatch::read(FontData::new(patch)).unwrap();
+        let patch_info = patch_info(IFT_TAG, 0);
+
+        let gvar = long_gvar_with_shared_tuples();
+
+        let font = test_font_for_patching_with_loca_mod(
+            true,
+            |_| {},
+            HashMap::from([
+                (Gvar::TAG, gvar.as_slice()),
+                (Tag::new(b"IFT "), vec![0, 0, 0, 0].as_slice()),
+            ]),
+        );
+        let font = FontRef::new(&font).unwrap();
+
+        let patched = apply_glyph_keyed_patches(&[(&patch_info, patch)], &font).unwrap();
+        let patched = FontRef::new(&patched).unwrap();
+
+        let new_gvar: &[u8] = patched.table_data(Gvar::TAG).unwrap().as_bytes();
+
+        let mut expected_gvar: Vec<u8> = vec![];
+
+        let change_start = gvar.offset_for("glyph_offset[3]");
+
+        expected_gvar.extend_from_slice(gvar.get(0..change_start).unwrap());
+        // Offsets
+        expected_gvar.extend_from_slice(&[
+            0x00, 0x00, 0x00, 0x06, // gid 3
+            0x00, 0x00, 0x00, 0x06, // gid 4
+            0x00, 0x00, 0x00, 0x06, // gid 5
+            0x00, 0x00, 0x00, 0x06, // gid 6
+            0x00, 0x00, 0x00, 0x06, // gid 7
+            0x00, 0x00, 0x00, 0x09, // gid 8
+            0x00, 0x00, 0x00, 0x0A, // gid 9
+            0x00, 0x00, 0x00, 0x0A, // gid 10
+            0x00, 0x00, 0x00, 0x0A, // gid 11
+            0x00, 0x00, 0x00, 0x0A, // gid 12
+            0x00, 0x00, 0x00, 0x0A, // gid 13
+            0x00, 0x00, 0x00, 0x0A, // gid 14
+            0x00, 0x00, 0x00, 0x0Au8, // trailing
+        ]);
+        // Shared tuples
+        expected_gvar.extend_from_slice(&[0, 42, 0, 13, 0, 25u8]);
+        // Data
+        expected_gvar.extend_from_slice(&[
+            1, 2, 3, 4, // gid 0
+            b'm', b'n', // gid 2
+            b'o', b'p', b'q', // gid 7
+            b'r', // gid 8
         ]);
         assert_eq!(&expected_gvar, new_gvar);
     }
@@ -1415,5 +1476,4 @@ pub(crate) mod tests {
     // - glyph keyed test with large number of offsets to check type conversion on (glyphCount * tableCount)
     // - test that glyph keyed patches are idempotent.
     // TODO XXXX gvar with overlapping glyph data and shared tuples
-    // TODO XXXX long offset gvar
 }
