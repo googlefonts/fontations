b2f2b1d3d3cd3921354b27ca6ac3ebce8639fbc5
Chad Brokaw <cbrokaw@gmail.com>
2025-03-07 12:35:22 -0500

    clippy

--- skrifa/src/attribute.rs
+++ skrifa/src/attribute.rs
@@ -30,7 +30,7 @@ pub struct Attributes {
 impl Attributes {
     /// Extracts the stretch, style and weight attributes for the default
     /// instance of the given font.
-    pub fn new<'a>(font: &FontRef<'a>) -> Self {
+    pub fn new(font: &FontRef) -> Self {
         if let Ok(os2) = font.os2() {
             // Prefer values from the OS/2 table if it exists. We also use
             // the post table to extract the angle for oblique styles.
