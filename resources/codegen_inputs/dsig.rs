79a14dc2b2f82a8e7a278505e8abe10084e30b84
Harry Dalton <harry.dalton@daltonmaag.com>
2025-08-14 18:10:02 +0100

    Add read and write for `DSIG` table (#1589)
    * Initial draft of read/write for the DSIG table
    
    * Test the dummy and full use case
    
    * Remove unnecessary manual length calculation
    
    * Remove unnecessary skip of field when traversing
    
    * Validate record format
    
    * Validate that the block fits within the outer record's prescribed length
    
    * Remove TODO comment resolved in review
    
    * Tidy block length calculation
    
    * Validate that the outer length field does not introduce ANY ambiguity
    
    * Provide DSIG in provider

---
+++ resources/codegen_inputs/dsig.rs
@@ -0,0 +1,62 @@
+#![parse_module(read_fonts::tables::dsig)]
+
+/// [DSIG (Digital Signature Table)](https://docs.microsoft.com/en-us/typography/opentype/spec/dsig#table-structure) table
+#[tag = "DSIG"]
+table Dsig {
+    /// Version number of the DSIG table (0x00000001)
+    #[compile(1)]
+    version: u32,
+
+    /// Number of signatures in the table
+    #[compile(array_len($signature_records))]
+    num_signatures: u16,
+
+    /// Permission flags
+    flags: PermissionFlags,
+
+    /// Array of signature records
+    #[count($num_signatures)]
+    signature_records: [SignatureRecord],
+}
+
+/// [Permission flags](https://learn.microsoft.com/en-us/typography/opentype/spec/dsig#table-structure)
+flags u16 PermissionFlags {
+    /// Bit 0: Cannot be resigned
+    CANNOT_BE_RESIGNED = 0b0000_0000_0000_0001,
+}
+
+/// [Signature Record](https://learn.microsoft.com/en-us/typography/opentype/spec/dsig#table-structure)
+record SignatureRecord {
+    /// Format of the signature
+    #[compile(1)]
+    format: u32,
+
+    /// Length of signature in bytes
+    #[compile(self.compute_signature_block_len())]
+    length: u32,
+    
+    /// Offset to the signature block from the beginning of the table
+    #[offset_getter(signature_block)]
+    signature_block_offset: Offset32<SignatureBlockFormat1>,
+}
+
+/// [Signature Block Format 1](https://learn.microsoft.com/en-us/typography/opentype/spec/dsig#table-structure)
+table SignatureBlockFormat1 {
+    /// Reserved for future use; set to zero.
+    #[skip_getter]
+    #[compile(0)]
+    _reserved1: u16,
+
+    /// Reserved for future use; set to zero.
+    #[skip_getter]
+    #[compile(0)]
+    _reserved2: u16,
+
+    /// Length (in bytes) of the PKCS#7 packet in the signature field.
+    #[compile(array_len($signature))]
+    signature_length: u32,
+
+    /// PKCS#7 packet
+    #[count($signature_length)]
+    signature: [u8],
+}
