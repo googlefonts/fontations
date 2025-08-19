aabe8e9d8a4763324ee48a7482d2e1046f8def72
Cosimo Lupo <clupo@google.com>
2025-08-19 16:48:30 +0100

    [release.sh] use a trap to catch SIGINT signal
    disabling errexit didn't work as I thought, CTRL-C would still kill the whole shell script (maybe the signal was being sent to the entire process group terminating not just cargo but the calling script as well, not sure).
    
    This other approach should in theory be more robust.

--- resources/scripts/release.sh
+++ resources/scripts/release.sh
@@ -15,6 +15,21 @@ function usage() {
   echo "Typically you should be running this after bump-version.sh"
 }
 
+handle_publish_interrupt() {
+    echo
+    echo "Publish interrupted by CTRL-C."
+    echo -n "Do you want to continue with tagging? [auto-continue in 5 seconds] (Y/n): "
+    if read -t 5 -r response; then
+        if [[ "$response" =~ ^[Nn]$ ]]; then
+            echo "Stopping release process."
+            exit 130  # Exit with SIGINT code
+        fi
+    else
+        # No input means 'Yes'; add newline after timeout
+        echo
+    fi
+}
+
 # What is it you want us to do?
 if [ $# -gt 1 ]; then
   die_with_usage "Specify 0 - meaning all - or 1 packages"
@@ -26,7 +41,7 @@ if [ $# -eq 1 ]; then
   crate_specifier="-p ${crates[0]}"
 fi
 
-# Do the thing. We set errexit so step failure should break us out.
+# Do the thing. We set errexit (in rel-common.sh) so step failure should break us out.
 
 echo "Dry run..."
 cargo release publish ${crate_specifier}
@@ -34,7 +49,12 @@ cargo release publish ${crate_specifier}
 echo "Doing the thing; ${COLOR_RED}PRESS CTRL+C if anything looks suspicious${TEXT_RESET}"
 
 echo "Publish to crates.io"
-cargo release publish -x ${crate_specifier}  # this prompts y/N
+
+# Set up a trap to handle CTRL-C during cargo release publish
+trap 'handle_publish_interrupt' SIGINT
+cargo release publish -x ${crate_specifier}
+trap - SIGINT  # Remove trap
+
 echo "Generate tags"
 cargo release tag -x ${crate_specifier}  # this prompts y/N
 echo "Pushing tag to github"
