74930ed6ff877d5f1115afed75cc3eafa5b92711
Behdad Esfahbod <behdad@behdad.org>
2026-02-04 12:26:42 -0700

    Fix stupid CI "typo" bug

--- skrifa/src/outline/varc/mod.rs
+++ skrifa/src/outline/varc/mod.rs
@@ -742,14 +742,14 @@ fn compute_sparse_region_scalar(region: &SparseVariationRegion<'_>, coords: &[F2
             continue;
         } else if coord < peak {
             // Use raw bits - scale factors cancel in the ratio
-            let numer = (coord.to_bits() - start.to_bits()) as f32;
+            let numerat = (coord.to_bits() - start.to_bits()) as f32;
             let denom = (peak.to_bits() - start.to_bits()) as f32;
-            scalar = scalar * numer / denom;
+            scalar = scalar * numerat / denom;
         } else {
             // Use raw bits - scale factors cancel in the ratio
-            let numer = (end.to_bits() - coord.to_bits()) as f32;
+            let numerat = (end.to_bits() - coord.to_bits()) as f32;
             let denom = (end.to_bits() - peak.to_bits()) as f32;
-            scalar = scalar * numer / denom;
+            scalar = scalar * numerat / denom;
         }
     }
     scalar
