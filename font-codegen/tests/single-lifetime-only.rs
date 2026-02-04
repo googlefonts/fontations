ff9afe7ff3bc0bcc8ee2f4b0ef48f51e394f73b5
Colin Rofls <colin@cmyr.net>
2026-02-04 14:26:08 -0500

    [codegen] Delete old tests
    These are orphaned files that date back to the very first codegen
    experiments, and have not been used in several years.

--- font-codegen/tests/single-lifetime-only.rs
+++
@@ -1,9 +0,0 @@
-font_types_macro::tables! {
-    SingleLifetimeOnly<'a, 'b> {
-        item_count: BigEndian<u16>,
-        #[count(item_count)]
-        items: [BigEndian<Uint24>],
-    }
-}
-
-fn main() {}
