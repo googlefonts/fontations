64fa01817e8fe91090722a791e02e68e1ddd4a41
Simon Cozens <simon@simon-cozens.org>
2026-02-10 22:18:20 +0000

    Subset fvar

--- skera/src/fvar.rs
+++ skera/src/fvar.rs
@@ -1,7 +1,11 @@
 //! impl subset() for fvar table
 
-use crate::{NameIdClosure, Plan};
-use write_fonts::read::tables::fvar::Fvar;
+use crate::{
+    serialize::SerializeErrorFlags, variations::solver::Triple, NameIdClosure, Plan, Subset,
+    SubsetError,
+};
+use skrifa::raw::{tables::fvar::InstanceRecord, TopLevelTable};
+use write_fonts::{read::tables::fvar::Fvar, types::Fixed};
 
 impl NameIdClosure for Fvar<'_> {
     //TODO: support partial-instancing
@@ -23,6 +27,138 @@ impl NameIdClosure for Fvar<'_> {
             }
         }
     }
+}
+
+impl Subset for Fvar<'_> {
+    fn subset(
+        &self,
+        plan: &Plan,
+        _font: &write_fonts::read::FontRef,
+        s: &mut crate::serialize::Serializer,
+        _builder: &mut write_fonts::FontBuilder,
+    ) -> Result<(), crate::SubsetError> {
+        if plan.axes_index_map.is_empty() {
+            return Err(SubsetError::SubsetTableError(Fvar::TAG)); // empty
+        }
+        subset_fvar(self, plan, s).map_err(|_| SubsetError::SubsetTableError(Fvar::TAG))
+    }
+}
+
+fn subset_fvar(
+    fvar: &Fvar<'_>,
+    plan: &Plan,
+    s: &mut crate::serialize::Serializer,
+) -> Result<(), SerializeErrorFlags> {
+    let old_axis_count = fvar.axis_count();
+    let new_axis_count = plan.axes_index_map.len() as u16;
+
+    // Version
+    s.embed(1_u16)?;
+    s.embed(0_u16)?;
+    let has_psname = fvar.instance_size() >= fvar.axis_count() * 4 + 6;
+    s.embed(16_u16)?; // Axes array offset
+    s.embed(2_u16)?; // reserved
+    s.embed(new_axis_count)?;
+    s.embed(20_u16)?; // axis size
+    let instances = fvar
+        .instances()
+        .map_err(|_| SerializeErrorFlags::SERIALIZE_ERROR_READ_ERROR)?;
+    let new_instance_coords = instances
+        .iter()
+        .flatten()
+        .map(|i| new_coords(&i, plan, old_axis_count as usize))
+        .collect::<Vec<_>>();
+    s.embed(new_instance_coords.iter().filter(|x| x.is_some()).count() as u16)?;
+    // Instance count
+    s.embed(new_axis_count * 4 + if has_psname { 6 } else { 4 })?; // instance size
+    for (ix, axis) in fvar
+        .axes()
+        .map_err(|_| SerializeErrorFlags::SERIALIZE_ERROR_READ_ERROR)?
+        .iter()
+        .enumerate()
+    {
+        if !plan.axes_index_map.contains_key(&ix) {
+            continue;
+        }
+        s.embed(axis.axis_tag())?;
+        if let Some(restricted_location) = plan.user_axes_location.get(&axis.axis_tag()) {
+            s.embed(Fixed::from_f64(restricted_location.minimum as f64))?;
+            s.embed(Fixed::from_f64(restricted_location.middle as f64))?;
+            s.embed(Fixed::from_f64(restricted_location.maximum as f64))?;
+        } else {
+            s.embed(axis.axis_tag())?;
+            s.embed(axis.min_value())?;
+            s.embed(axis.default_value())?;
+            s.embed(axis.max_value())?;
+        }
+        s.embed(axis.flags())?;
+        s.embed(axis.axis_name_id())?;
+    }
+
+    for (instance_record, new_coords) in instances
+        .iter()
+        .flatten()
+        .zip(new_instance_coords.into_iter())
+    {
+        if let Some(coords) = new_coords {
+            s.embed(instance_record.subfamily_name_id)?;
+            s.embed(instance_record.flags)?;
+            for coord in coords {
+                s.embed(coord)?;
+            }
+            if let Some(name_id) = instance_record.post_script_name_id {
+                s.embed(name_id)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn new_coords(
+    instance: &InstanceRecord<'_>,
+    plan: &Plan,
+    old_axis_count: usize,
+) -> Option<Vec<Fixed>> {
+    let coords = instance.coordinates;
+    let axis_location = &plan.user_axes_location;
+    let mut new_coords = vec![];
+    if plan.axes_location.is_empty() {
+        return None;
+    }
+    for axis in 0..old_axis_count {
+        let tag = plan.axes_old_index_tag_map.get(&axis)?;
+        let coord = coords[axis];
+        if let Some(restricted_location) = axis_location.get(tag) {
+            if !axis_coord_pinned_or_within_axis_range(
+                coords.get(axis).map(|x| x.get()),
+                restricted_location,
+            ) {
+                return None;
+            }
+            if restricted_location.is_point() {
+                continue;
+            }
+        }
+        new_coords.push(coord.get());
+    }
+    Some(new_coords)
+}
+
+fn axis_coord_pinned_or_within_axis_range(coord: Option<Fixed>, axis_limit: &Triple) -> bool {
+    if let Some(coord) = coord {
+        let axis_coord = coord.to_f32();
+        if axis_limit.is_point() {
+            if axis_limit.minimum != axis_coord {
+                return false;
+            }
+        } else {
+            if axis_coord < axis_limit.minimum || axis_coord > axis_limit.maximum {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 #[cfg(test)]
