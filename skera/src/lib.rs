20450b6416936e4ee8ef867ace2f9c7018c53af1
Simon Cozens <simon@simon-cozens.org>
2026-02-10 14:50:08 +0000

    Apply instancing spec to plan

--- skera/src/lib.rs
+++ skera/src/lib.rs
@@ -39,6 +39,7 @@ mod vmtx;
 mod vorg;
 mod vvar;
 use crate::{
+    parsing_util::InstancingSpec,
     repack::resolve_overflows,
     variations::solver::{Triple, TripleDistances},
 };
@@ -424,6 +425,7 @@ impl Plan {
         layout_features: &IntSet<Tag>,
         name_ids: &IntSet<NameId>,
         name_languages: &IntSet<u16>,
+        variations: &Option<InstancingSpec>,
     ) -> Self {
         let mut this = Plan {
             glyphs_requested: input_gids.clone(),
@@ -437,6 +439,10 @@ impl Plan {
             pinned_at_default: true,
             ..Default::default()
         };
+
+        if let Some(variations) = variations {
+            let _ = this.apply_instancing_spec(variations, font); // XXX Propagate
+        }
 
         // ref: <https://github.com/harfbuzz/harfbuzz/blob/b5a65e0f20c30a7f13b2f6619479a6d666e603e0/src/hb-subset-input.cc#L71>
         let default_no_subset_tables = [gasp::Gasp::TAG, FPGM, PREP, VDMX, DSIG];
@@ -1012,6 +1018,66 @@ impl Plan {
             &mut self.base_varstore_inner_maps,
         );
     }
+
+    fn apply_instancing_spec(
+        &mut self,
+        spec: &InstancingSpec,
+        font: &FontRef,
+    ) -> Result<(), SubsetError> {
+        if spec.pin_all_axes_to_default {
+            for axis in font.axes().iter() {
+                self.user_axes_location
+                    .insert(axis.tag(), Triple::point(axis.default_value().into()));
+            }
+            return Ok(());
+        }
+        for font_axis in font.axes().iter() {
+            let tag = font_axis.tag();
+            let spec_axis = spec.axes.get(&tag);
+            match spec_axis {
+                Some(parsing_util::AxisSpec::PinToDefault) => {
+                    self.user_axes_location
+                        .insert(tag, Triple::point(font_axis.default_value().into()));
+                }
+                Some(parsing_util::AxisSpec::Range { min, def, max }) => {
+                    let min = if min.is_nan() {
+                        font_axis.min_value()
+                    } else {
+                        *min
+                    };
+                    let max = if max.is_nan() {
+                        font_axis.max_value()
+                    } else {
+                        *max
+                    };
+                    let def = if def.is_nan() {
+                        font_axis.default_value()
+                    } else {
+                        *def
+                    };
+                    let new_min = min.clamp(font_axis.min_value(), font_axis.max_value());
+                    let new_max = max.clamp(font_axis.min_value(), font_axis.max_value());
+                    let new_def = def.clamp(new_min, new_max);
+                    self.user_axes_location.insert(
+                        tag,
+                        Triple::new(new_min as f64, new_def as f64, new_max as f64),
+                    );
+                }
+                None => {
+                    // If an axis is not specified in the instancing spec, we keep it as is, which means it's not pinned and will not be removed.
+                    self.user_axes_location.insert(
+                        tag,
+                        Triple::new(
+                            font_axis.min_value().into(),
+                            font_axis.default_value().into(),
+                            font_axis.max_value().into(),
+                        ),
+                    );
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 // TODO: when instancing, calculate delta value and set new varidx to NO_VARIATIONS_IDX if all axes are pinned
@@ -1215,6 +1281,9 @@ pub enum SubsetError {
 
     #[error("Subsetting table '{0}' failed")]
     SubsetTableError(Tag),
+
+    #[error("Invalid input to --variations: {0}")]
+    InvalidInstancingSpec(String),
 }
 
 pub trait NameIdClosure {
@@ -1727,5 +1796,30 @@ mod test {
         assert!(plan.glyphset.contains(GlyphId::new(2)));
         assert!(plan.glyphset.contains(GlyphId::new(4)));
         assert!(plan.glyphset.contains(GlyphId::new(7)));
+    }
+
+    #[test]
+    fn test_axes_location() {
+        let mut plan = Plan::default();
+        let font = FontRef::new(include_bytes!("../test-data/fonts/NotoSans-VF.abc.ttf")).unwrap();
+        let spec = parse_instancing_spec("wdth=80:90").unwrap();
+        plan.apply_instancing_spec(&spec, &font).unwrap();
+        plan.normalize_axes_location(&font).unwrap();
+        let triple = plan.axes_location.get(&Tag::new(b"wdth")).unwrap();
+        assert!(
+            (triple.minimum - -0.566650).abs() < 0.00001,
+            "Expected minimum to be approximately -0.566650, got {}",
+            triple.minimum
+        );
+        assert!(
+            (triple.middle - -0.293335).abs() < 0.00001,
+            "Expected middle to be approximately -0.293335, got {}",
+            triple.middle
+        );
+        assert!(
+            (triple.maximum - -0.293335).abs() < 0.00001,
+            "Expected maximum to be approximately -0.293335, got {}",
+            triple.maximum
+        );
     }
 }
