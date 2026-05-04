5fe0b05e8d7721a93319775a2ce1779a97c23110
Chad Brokaw <cbrokaw@gmail.com>
2026-05-03 21:55:36 -0400

    add shaping data interop test

--- read-fonts/src/model/font/interop.rs
+++ read-fonts/src/model/font/interop.rs
@@ -14,5 +14,21 @@ pub fn _get_or_init_shaping_data(
     font: &Font,
     f: impl FnOnce() -> Box<dyn Any + Send + Sync>,
 ) -> &dyn Any {
-    &*font.0.shaping_data.get_or_init(|| f())
+    &**font.0.shaping_data.get_or_init(|| f())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn recover_shaping_data() {
+        struct ShapingData {
+            arg: i32,
+        }
+        let font = Font::new(font_test_data::AHEM, 0).unwrap();
+        let data = _get_or_init_shaping_data(&font, || Box::new(ShapingData { arg: -42 }));
+        let actual = data.downcast_ref::<ShapingData>().unwrap();
+        assert_eq!(actual.arg, -42);
+    }
 }
