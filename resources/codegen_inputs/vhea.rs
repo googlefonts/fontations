99b13c862b074f3eb49cedb102702e4fd997d57c
Simon Cozens <simon@simon-cozens.org>
2026-07-21 10:07:55 +0100

    Default vhea table to version 1.1, but allow changes

--- resources/codegen_inputs/vhea.rs
+++ resources/codegen_inputs/vhea.rs
@@ -3,8 +3,8 @@
 /// The [vhea](https://docs.microsoft.com/en-us/typography/opentype/spec/vhea) Vertical Header Table
 #[tag = "vhea"]
 table Vhea {
-    /// The major/minor version (1, 1)
-    #[compile(Version16Dot16::VERSION_1_1)]
+    /// The major/minor version (1, 0 or 1, 1)
+    #[default(Version16Dot16::VERSION_1_1)]
     version: Version16Dot16,
     /// Typographic ascent.
     ascender: FWord,
